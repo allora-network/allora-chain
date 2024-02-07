eb47a6c40198e5a6cedb5c32680cf45681a0c872
Derek Anderson <dmikey@users.noreply.github.com>
2024-02-07 09:16:08 -0600

    add a faucet account to the test init (#37)
    * Update init.sh
    
    add a faucet account
    
    * Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,6 +14,8 @@ go run ./cmd/uptd genesis add-genesis-account bob 1000upt --keyring-backend test
 go run ./cmd/uptd genesis add-genesis-account upt1wg289gh4tv3en5g49pex433hwnsld6yzzhppgv 6969upt
 go run ./cmd/uptd genesis add-genesis-account upt182rv3s5jvn5v7lxaz3elpwzgyl7zqvqe727ee5 1337upt
 go run ./cmd/uptd genesis add-genesis-account upt1acgw83473tp35wnxutpvhwadzlgull0vvf7stj 4200upt
+go run ./cmd/uptd genesis add-genesis-account upt1ey0fvvpx3y99g7s8n8k7ft74dh0zq6y7j86ff8 10000000upt
+
 # create default validator
 go run ./cmd/uptd genesis gentx alice 1000000upt --chain-id demo
 go run ./cmd/uptd genesis collect-gentxs
