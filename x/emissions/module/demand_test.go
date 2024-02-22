7c01822e0b45b5d81113b10a03be7b1b5d8f828a
Tyler <tyler@cyb3r.space>
2024-02-21 21:29:35 -0500

    test isValidAtPrice

--- x/emissions/module/demand_test.go
+++ x/emissions/module/demand_test.go
@@ -1,6 +1,8 @@
 package module_test
 
 import (
+	"time"
+
 	cosmosMath "cosmossdk.io/math"
 	state "github.com/allora-network/allora-chain/x/emissions"
 	"github.com/allora-network/allora-chain/x/emissions/keeper"
@@ -25,4 +27,119 @@ func (s *ModuleTestSuite) TestInactivateLowDemandTopicsRemoveOneTopicLeaveOne() 
 	s.Require().NoError(err, "InactivateLowDemandTopics should not throw an error")
 	s.Require().Len(listTopics, 1, "InactivateLowDemandTopics should return 0 topics")
 	s.Require().Equal(createdTopicIds[0], (*listTopics[0]).Id, "InactivateLowDemandTopics should match expected")
+}
+
+func (s *ModuleTestSuite) TestIsValidAtPriceTrue() {
+	price := cosmosMath.NewUint(100)
+	currentTime := uint64(time.Now().UTC().Unix())
+	req := state.InferenceRequest{
+		Sender:               s.addrsStr[0],
+		Nonce:                0,
+		TopicId:              1,
+		Cadence:              0,
+		MaxPricePerInference: price,
+		BidAmount:            price,
+		LastChecked:          currentTime - 100,
+		TimestampValidUntil:  currentTime + 100,
+		ExtraData:            []byte("Test"),
+	}
+	reqId, err := req.GetRequestId()
+	s.Require().NoError(err, "GetRequestId should not throw an error")
+	err = s.emissionsKeeper.SetRequestDemand(s.ctx, reqId, price.Add(cosmosMath.NewUint(1)))
+	s.Require().NoError(err, "SetRequestDemand should not throw an error")
+	valid, err := module.IsValidAtPrice(s.ctx, s.appModule, req, price, currentTime)
+	s.Require().NoError(err, "IsValidAtPrice should not throw an error")
+	s.Require().True(valid, "IsValidAtPrice should return true")
+}
+
+func (s *ModuleTestSuite) TestIsValidAtPriceFalseCadenceNotYetReady() {
+	price := cosmosMath.NewUint(100)
+	currentTime := uint64(time.Now().UTC().Unix())
+	req := state.InferenceRequest{
+		Sender:               s.addrsStr[0],
+		Nonce:                0,
+		TopicId:              1,
+		Cadence:              currentTime + 3000,
+		MaxPricePerInference: price,
+		BidAmount:            price,
+		LastChecked:          currentTime - 100,
+		TimestampValidUntil:  currentTime + 100,
+		ExtraData:            []byte("Test"),
+	}
+	reqId, err := req.GetRequestId()
+	s.Require().NoError(err, "GetRequestId should not throw an error")
+	err = s.emissionsKeeper.SetRequestDemand(s.ctx, reqId, price.Add(cosmosMath.NewUint(1)))
+	s.Require().NoError(err, "SetRequestDemand should not throw an error")
+	valid, err := module.IsValidAtPrice(s.ctx, s.appModule, req, price, currentTime)
+	s.Require().NoError(err, "IsValidAtPrice should not throw an error")
+	s.Require().False(valid, "IsValidAtPrice should return false")
+}
+
+func (s *ModuleTestSuite) TestIsValidAtPriceFalseTimestampNoLongerValid() {
+	price := cosmosMath.NewUint(100)
+	currentTime := uint64(time.Now().UTC().Unix())
+	req := state.InferenceRequest{
+		Sender:               s.addrsStr[0],
+		Nonce:                0,
+		TopicId:              1,
+		Cadence:              0,
+		MaxPricePerInference: price,
+		BidAmount:            price,
+		LastChecked:          currentTime - 100,
+		TimestampValidUntil:  currentTime - 100,
+		ExtraData:            []byte("Test"),
+	}
+	reqId, err := req.GetRequestId()
+	s.Require().NoError(err, "GetRequestId should not throw an error")
+	err = s.emissionsKeeper.SetRequestDemand(s.ctx, reqId, price.Add(cosmosMath.NewUint(1)))
+	s.Require().NoError(err, "SetRequestDemand should not throw an error")
+	valid, err := module.IsValidAtPrice(s.ctx, s.appModule, req, price, currentTime)
+	s.Require().NoError(err, "IsValidAtPrice should not throw an error")
+	s.Require().False(valid, "IsValidAtPrice should return false")
+}
+
+func (s *ModuleTestSuite) TestIsValidAtPriceFalseNotEnoughReqDemand() {
+	price := cosmosMath.NewUint(100)
+	currentTime := uint64(time.Now().UTC().Unix())
+	req := state.InferenceRequest{
+		Sender:               s.addrsStr[0],
+		Nonce:                0,
+		TopicId:              1,
+		Cadence:              0,
+		MaxPricePerInference: price,
+		BidAmount:            price,
+		LastChecked:          currentTime - 100,
+		TimestampValidUntil:  currentTime + 100,
+		ExtraData:            []byte("Test"),
+	}
+	reqId, err := req.GetRequestId()
+	s.Require().NoError(err, "GetRequestId should not throw an error")
+	err = s.emissionsKeeper.SetRequestDemand(s.ctx, reqId, price.Sub(cosmosMath.NewUint(1)))
+	s.Require().NoError(err, "SetRequestDemand should not throw an error")
+	valid, err := module.IsValidAtPrice(s.ctx, s.appModule, req, price, currentTime)
+	s.Require().NoError(err, "IsValidAtPrice should not throw an error")
+	s.Require().False(valid, "IsValidAtPrice should return false")
+}
+
+func (s *ModuleTestSuite) TestIsValidAtPriceFalseMaxPriceTooHigh() {
+	price := cosmosMath.NewUint(100)
+	currentTime := uint64(time.Now().UTC().Unix())
+	req := state.InferenceRequest{
+		Sender:               s.addrsStr[0],
+		Nonce:                0,
+		TopicId:              1,
+		Cadence:              0,
+		MaxPricePerInference: price.Sub(cosmosMath.OneUint()),
+		BidAmount:            price,
+		LastChecked:          currentTime - 100,
+		TimestampValidUntil:  currentTime + 100,
+		ExtraData:            []byte("Test"),
+	}
+	reqId, err := req.GetRequestId()
+	s.Require().NoError(err, "GetRequestId should not throw an error")
+	err = s.emissionsKeeper.SetRequestDemand(s.ctx, reqId, price.Add(cosmosMath.NewUint(1)))
+	s.Require().NoError(err, "SetRequestDemand should not throw an error")
+	valid, err := module.IsValidAtPrice(s.ctx, s.appModule, req, price, currentTime)
+	s.Require().NoError(err, "IsValidAtPrice should not throw an error")
+	s.Require().False(valid, "IsValidAtPrice should return false")
 }
