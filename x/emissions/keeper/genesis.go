9a3284a4a746cc5c901fe2435d00665369c1d9d7
Guilherme Brandão <guilherme1237@gmail.com>
2024-02-23 12:10:37 +0000

    Added core team addresses

--- x/emissions/keeper/genesis.go
+++ x/emissions/keeper/genesis.go
@@ -30,19 +30,9 @@ func (k *Keeper) InitGenesis(ctx context.Context, data *state.GenesisState) erro
 		return err
 	}
 
-	// Add the core team to the whitelists
-	coreTeamAddresses := []string{
-		"allo1srjcynn6jw5l709upwwx70gs3eyjdmkgufcqdt",
-	}
-	// Map these addresses to AccAddress
-	for _, addr := range coreTeamAddresses {
-		accAddress, err := sdk.AccAddressFromBech32(addr)
-		if err != nil {
-			return err
-		}
-		k.AddWhitelistAdmin(ctx, accAddress)
-		k.AddToTopicCreationWhitelist(ctx, accAddress)
-		k.AddToWeightSettingWhitelist(ctx, accAddress)
+	// add core team to the whitelists
+	if err := k.addCoreTeamToWhitelists(ctx); err != nil {
+		return err
 	}
 
 	return nil
@@ -58,4 +48,31 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*state.GenesisState, error)
 	return &state.GenesisState{
 		Params: params,
 	}, nil
+}
+
+func (k *Keeper) addCoreTeamToWhitelists(ctx context.Context) error {
+	coreTeamAddresses := []string{
+		"allo1srjcynn6jw5l709upwwx70gs3eyjdmkgufcqdt",
+		"allo1uu2fk9gmjkmy8qme46w8hr8j2exfjnsqnweznz",
+		"allo1fk0pt0fj03fxdyyq0mhj5cfq5kv9ncwmtqvy7u",
+		"allo1d6hegr59ftc43xxd2n2vn5ndley699jefwdgq8",
+		"allo1xw354nrfpsw6x3sf7aqnrek0wluyx58zv2uc75",
+		"allo1d54qdljsc3srsy8fz6zzrx90hyzuevnccg88md",
+		"allo1fumaxxyjwdv7y5uh6wxslhxmqf32jtvnr8edlf",
+		"allo1memyg7exjzjdpdv98cfm6u0y0lsz4ev8mk57hc",
+		"allo1shzv768qrxaextjwz0aj6nzhm3cyy4pdug8jy6",
+		"allo12heywwqc75mgk6qg3n0mryw58jn6ujtp7tfvs9",
+	}
+
+	for _, addr := range coreTeamAddresses {
+		accAddress, err := sdk.AccAddressFromBech32(addr)
+		if err != nil {
+			return err
+		}
+		k.AddWhitelistAdmin(ctx, accAddress)
+		k.AddToTopicCreationWhitelist(ctx, accAddress)
+		k.AddToWeightSettingWhitelist(ctx, accAddress)
+	}
+
+	return nil
 }
