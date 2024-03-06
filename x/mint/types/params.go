338fecc87ae2941cd3356b65cfc8875d61383f84
Guilherme Brandão <guilherme1237@gmail.com>
2024-03-06 00:33:17 +0000

    Remove unnecessary validation

--- x/mint/types/params.go
+++ x/mint/types/params.go
@@ -99,9 +99,6 @@ func validateInflationRateChange(i interface{}) error {
 	if v.IsNegative() {
 		return fmt.Errorf("inflation rate change cannot be negative: %s", v)
 	}
-	if v.GT(math.LegacyOneDec()) {
-		return fmt.Errorf("inflation rate change too large: %s", v)
-	}
 
 	return nil
 }
@@ -117,9 +114,6 @@ func validateInflationMax(i interface{}) error {
 	}
 	if v.IsNegative() {
 		return fmt.Errorf("max inflation cannot be negative: %s", v)
-	}
-	if v.GT(math.LegacyOneDec()) {
-		return fmt.Errorf("max inflation too large: %s", v)
 	}
 
 	return nil
