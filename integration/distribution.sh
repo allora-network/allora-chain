b371a7d963e36c6a5881c871cc8ef5ba9f652ecf
Tyler <tyler@cyb3r.space>
2024-03-11 11:49:32 -0400

    just make an integration test in bash you idiot

--- integration/distribution.sh
+++ integration/distribution.sh
@@ -19,21 +19,37 @@ if [[ ${#VALIDATOR_ADDRESS} -ne 50 ]] || [[ $VALIDATOR_ADDRESS != allovaloper* ]
     exit 1
 fi
 
+ALLORA_REWARDS_ADDRESS=$($ALLORAD_BIN query auth module-account "allorarewards" | grep "address: allo" | cut -f 2 -d ":" | tr -d " " | tr -d "\"")
+if [[ ${#ALLORA_REWARDS_ADDRESS} -ne 43 ]] || [[ $ALLORA_REWARDS_ADDRESS != allo* ]]; then
+    echo "Allora rewards address not found"
+    exit 1
+fi
+
 # get the current outstanding rewards
 DISTRIBUTION_REWARDS_0=$($ALLORAD_BIN query distribution validator-outstanding-rewards $VALIDATOR_ADDRESS | grep "amount" | cut -f 2 -d ":" | tr -d " " | tr -d "\"")
+ALLORA_REWARDS_0=$($ALLORAD_BIN query bank balances $ALLORA_REWARDS_ADDRESS | grep "amount" | cut -f 2 -d ":" | tr -d " " | tr -d "\"")
 
 # wait for some blocks to get mined
 sleep 5
 
 # get the current outstanding rewards
 DISTRIBUTION_REWARDS_1=$($ALLORAD_BIN query distribution validator-outstanding-rewards $VALIDATOR_ADDRESS | grep "amount" | cut -f 2 -d ":" | tr -d " " | tr -d "\"")
+ALLORA_REWARDS_1=$($ALLORAD_BIN query bank balances $ALLORA_REWARDS_ADDRESS | grep "amount" | cut -f 2 -d ":" | tr -d " " | tr -d "\"")
 
 # assert that the rewards have increased
-INCREASED=$(bc <<< "$DISTRIBUTION_REWARDS_1 > $DISTRIBUTION_REWARDS_0")
+DISTRIBUTION_INCREASED=$(bc <<< "$DISTRIBUTION_REWARDS_1 > $DISTRIBUTION_REWARDS_0")
+ALLORA_REWARDS_INCREASED=$(bc <<< "$ALLORA_REWARDS_1 > $ALLORA_REWARDS_0")
 
-if [[ $INCREASED -ne 1 ]]; then
+if [[ $DISTRIBUTION_INCREASED -ne 1 ]]; then
     echo "Distribution of rewards to validators did not increase"
     exit 1
 else 
     echo "Distribution of rewards to validators increased"
+fi
+
+if [[ $ALLORA_REWARDS_INCREASED -ne 1 ]]; then
+    echo "Distribution of rewards to allora rewards did not increase"
+    exit 1
+else 
+    echo "Distribution of rewards to allora rewards increased"
 fi
