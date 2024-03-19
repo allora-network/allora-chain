be657fc989f087dfd0750fd047382550008b3e2f
Derek Anderson <derek@upshot.xyz>
2024-03-19 18:55:19 -0500

    change org
    Signed-off-by: Derek Anderson <derek@upshot.xyz>

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the base URL and application name
-BASE_URL="https://github.com/dmikey/allora-chain/releases/download/v0.0.3"
+BASE_URL="https://github.com/allora-network/allora-chain/releases/download/v0.0.3"
 APP_NAME="allorad"
 
 # Determine the operating system and architecture
