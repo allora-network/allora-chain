80fa3c13d098356384751cc593a803241d7d365f
Michael <michael.c@upshot.xyz>
2024-03-21 17:32:38 -0500

    setup relayer for testing ibc bridge

---
+++ x/ibc/scripts/relayer/rly-link.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configure predefined mnemonic pharses
+BINARY=./rly
+CHAIN_DIR=./data
+RELAYER_DIR=./relayer
+
+# Ensure rly is installed
+if ! [ -x "$(command -v $BINARY)" ]; then
+    echo "$BINARY is required to run this script..."
+    echo "You can download at https://github.com/cosmos/relayer"
+    exit 1
+fi
+
+echo "Linking both chains"
+$BINARY tx link alloard-axelar --home $CHAIN_DIR/$RELAYER_DIR
