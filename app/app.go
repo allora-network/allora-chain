d72fe29ea3c30a8f961b546fbd3eb60b3e055c77
Guilherme Brandão <37072140+guilherme-brandao@users.noreply.github.com>
2024-03-22 14:25:27 +0000

    Added slashing module (#88)

--- app/app.go
+++ app/app.go
@@ -40,6 +40,8 @@ import (
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
+	slashingkeeper "github.com/cosmos/cosmos-sdk/x/slashing/keeper"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	capabilitykeeper "github.com/cosmos/ibc-go/modules/capability/keeper"
 	capabilitytypes "github.com/cosmos/ibc-go/modules/capability/types"
@@ -62,6 +64,7 @@ import (
 	_ "github.com/cosmos/cosmos-sdk/x/consensus"             // import for side-effects
 	_ "github.com/cosmos/cosmos-sdk/x/distribution"          // import for side-effects
 	_ "github.com/cosmos/cosmos-sdk/x/params"                // import for side-effects
+	_ "github.com/cosmos/cosmos-sdk/x/slashing"              // import for side-effects
 	_ "github.com/cosmos/cosmos-sdk/x/staking"               // import for side-effects
 )
 
@@ -96,6 +99,7 @@ type AlloraApp struct {
 	EmissionsKeeper       emissionsKeeper.Keeper
 	ParamsKeeper          paramskeeper.Keeper
 	UpgradeKeeper         *upgradekeeper.Keeper
+	SlashingKeeper        slashingkeeper.Keeper
 
 	// IBC
 	IBCKeeper           *ibckeeper.Keeper // IBC Keeper must be a pointer in the app, so we can SetRouter on it correctly
@@ -173,6 +177,7 @@ func NewAlloraApp(
 		&app.EmissionsKeeper,
 		&app.UpgradeKeeper,
 		&app.ParamsKeeper,
+		&app.SlashingKeeper,
 	); err != nil {
 		return nil, err
 	}
@@ -195,6 +200,7 @@ func NewAlloraApp(
 		capabilitytypes.ModuleName,
 		emissions.ModuleName,
 		distrtypes.ModuleName,
+		slashingtypes.ModuleName,
 		stakingtypes.ModuleName,
 		minttypes.ModuleName,
 		ibcexported.ModuleName,
