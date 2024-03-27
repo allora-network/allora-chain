438dcc36a572fb26ca39990af454ef3119d74261
Tyler <tyler@cyb3r.space>
2024-03-27 11:43:22 -0400

    fix inference request integration test

--- integration/inferencerequest.sh
+++ integration/inferencerequest.sh
@@ -8,17 +8,18 @@ source $(dirname $0)/common.sh
 echo
 echo "Creating a request for inference on topic 1"
 
+BLOCK_HEIGHT_CURR=$($ALLORAD_BIN query consensus comet block-latest | grep "height" | head -n 1 | cut -d ":" -f 2 | tr -d " " | tr -d "\"")
 RI_CREATOR="$BOB_ADDRESS"
 RI_NONCE="1"
 RI_TOPIC_ID="1"
 RI_CADENCE="60"
 RI_MAX_PRICE_PER_INFERENCE="1"
 RI_BID_AMOUNT="10000"
-RI_TIMESTAMP_VALID_UNTIL=$(($(date +%s)+60*60*24))
+RI_BLOCK_VALID_UNTIL=$(bc <<< "$BLOCK_HEIGHT_CURR + 61")
 $ALLORAD_BIN tx emissions request-inference \
   $RI_CREATOR \
-  "{\"nonce\": \"$RI_NONCE\",\"topic_id\":\"$RI_TOPIC_ID\",\"cadence\":\"$RI_CADENCE\",\"max_price_per_inference\":\"$RI_MAX_PRICE_PER_INFERENCE\",\"bid_amount\":\"$RI_BID_AMOUNT\",\"timestamp_valid_until\":\"$RI_TIMESTAMP_VALID_UNTIL\"}" \
-  --yes --keyring-backend=test --chain-id=demo --gas-prices=1uallo --gas=auto --gas-adjustment=1.5;
+  "{\"nonce\": \"$RI_NONCE\",\"topic_id\":\"$RI_TOPIC_ID\",\"cadence\":\"$RI_CADENCE\",\"max_price_per_inference\":\"$RI_MAX_PRICE_PER_INFERENCE\",\"bid_amount\":\"$RI_BID_AMOUNT\",\"block_valid_until\":\"$RI_BLOCK_VALID_UNTIL\",\"extra_data\":\"\"}" \
+  --yes --keyring-backend=test --chain-id=demo; #--gas-prices=1uallo --gas=auto --gas-adjustment=1.5;
 
 echo "Checking the inference request was made correctly"
 
@@ -45,7 +46,7 @@ fi
 echo
 echo "reactivating the topic so the request will fire"
 
-$ALLORAD_BIN tx emissions reactivate-topic $RI_CREATOR 1 --yes --gas-prices=1uallo --gas=auto --gas-adjustment=1.5;
+$ALLORAD_BIN tx emissions reactivate-topic $RI_CREATOR 1 --yes;
 sleep 3
 
 echo "Checking that the allora daemon is still running"
