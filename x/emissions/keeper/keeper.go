927925868bcd87a0f027eaadece7404f34e264c0
Guilherme Brandão <guilherme1237@gmail.com>
2024-03-28 19:07:46 +0000

    Added worker rewards test

--- x/emissions/keeper/keeper.go
+++ x/emissions/keeper/keeper.go
@@ -559,8 +559,14 @@ func (k *Keeper) GetValueBundles(ctx context.Context, topicId TOPIC_ID, block BL
 	return &valueBundles, nil
 }
 
-// A function that accepts a topicId and returns the latest Network ValueBundle or error
-func (k *Keeper) GetLatestNetworkValueBundle(ctx context.Context, topicId TOPIC_ID) (*types.ValueBundle, error) {
+// Insert a network loss bundle for a topic and block.
+func (k *Keeper) InsertNetworkLossBundle(ctx context.Context, topicId TOPIC_ID, block BLOCK_NUMBER, lossBundle types.LossBundle) error {
+	key := collections.Join(topicId, block)
+	return k.networkLossBundles.Set(ctx, key, lossBundle)
+}
+
+// A function that accepts a topicId and returns the latest Network LossBundle or error
+func (k *Keeper) GetLatestNetworkLossBundle(ctx context.Context, topicId TOPIC_ID) (*types.LossBundle, error) {
 	// Parse networkLossBundles for the topicId in descending time order and take the first one
 	rng := collections.
 		NewPrefixedPairRange[TOPIC_ID, BLOCK_NUMBER](topicId).
