bc5900555d64068b44fceb9a75826661db4ede8d
br4e <milesminton@gmail.com>
2024-04-02 16:23:06 -0700

    test_equations_3_through_8

---
+++ x/emissions/module/network_summaries_test.go
@@ -0,0 +1,84 @@
+package module_test
+
+import (
+	"math"
+
+	"github.com/allora-network/allora-chain/x/emissions/module"
+	"github.com/allora-network/allora-chain/x/emissions/types"
+)
+
+func (s *ModuleTestSuite) TestGradient() {
+	// Define test cases
+	tests := []struct {
+		name        string
+		p           float64
+		x           float64
+		expected    float64
+		expectedErr error
+	}{
+		{
+			name:        "normal operation",
+			p:           2,
+			x:           1,
+			expected:    1.92014,
+			expectedErr: nil,
+		},
+		{
+			name:        "normal operation",
+			p:           10,
+			x:           3,
+			expected:    216664,
+			expectedErr: nil,
+		},
+		{
+			name:        "normal operation",
+			p:           9.2,
+			x:           3.4,
+			expected:    219724,
+			expectedErr: nil,
+		},
+		{
+			name:        "p is NaN",
+			p:           math.NaN(),
+			x:           1,
+			expected:    0,
+			expectedErr: types.ErrPhiInvalidInput,
+		},
+		{
+			name:        "x is NaN",
+			p:           2,
+			x:           math.NaN(),
+			expected:    0,
+			expectedErr: types.ErrPhiInvalidInput,
+		},
+		{
+			name:        "p is Inf",
+			p:           math.Inf(1),
+			x:           1,
+			expected:    0,
+			expectedErr: types.ErrPhiInvalidInput,
+		},
+		{
+			name:        "x is Inf",
+			p:           1,
+			x:           math.Inf(1),
+			expected:    0,
+			expectedErr: types.ErrPhiInvalidInput,
+		},
+	}
+
+	for _, tc := range tests {
+		s.Run(tc.name, func() {
+			// Call the function under test
+			result, err := module.Gradient(tc.p, tc.x)
+
+			// Validate the results
+			if tc.expectedErr != nil {
+				s.Require().ErrorIs(err, tc.expectedErr)
+			} else {
+				s.Require().NoError(err)
+				s.Require().InEpsilon(tc.expected, result, 1e-5, "result should match expected value within epsilon")
+			}
+		})
+	}
+}
