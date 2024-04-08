48690c72314824496a03f69f282e4ba768bd2172
br4e <milesminton@gmail.com>
2024-04-08 16:55:34 -0500

    Update forecast_implied_inferences_test.go

--- x/emissions/module/inference_synthesis/forecast_implied_inferences_test.go
+++ x/emissions/module/inference_synthesis/forecast_implied_inferences_test.go
@@ -118,41 +118,39 @@ func (s *InferenceSynthesisTestSuite) TestCalcForcastImpliedInferences() {
 			},
 			expectedErr: nil,
 		},
-		/*
-			{ // ROW 2
-				name: "basic functionality, two workers, two forecasters",
-				inferenceByWorker: map[string]*emissions.Inference{
-					"worker0": {Value: -0.2797477698393250},
-					"worker1": {Value: 0.26856211587161100},
-				},
-				forecasts: &emissions.Forecasts{
-					Forecasts: []*emissions.Forecast{
-						{
-							Forecaster: "forecaster0",
-							ForecastElements: []*emissions.ForecastElement{
-								{Inferer: "worker0", Value: 0.02089366880023640},
-								{Inferer: "worker1", Value: 0.3342267861383700},
-							},
+		{ // ROW 2
+			name: "basic functionality, two workers, two forecasters",
+			inferenceByWorker: map[string]*emissions.Inference{
+				"worker0": {Value: -0.2797477698393250},
+				"worker1": {Value: 0.26856211587161100},
+			},
+			forecasts: &emissions.Forecasts{
+				Forecasts: []*emissions.Forecast{
+					{
+						Forecaster: "forecaster0",
+						ForecastElements: []*emissions.ForecastElement{
+							{Inferer: "worker0", Value: 0.02089366880023640},
+							{Inferer: "worker1", Value: 0.3342267861383700},
 						},
-						{
-							Forecaster: "forecaster1",
-							ForecastElements: []*emissions.ForecastElement{
-								{Inferer: "worker0", Value: 0.042947662325033700},
-								{Inferer: "worker1", Value: 0.2816951641179930},
-							},
+					},
+					{
+						Forecaster: "forecaster1",
+						ForecastElements: []*emissions.ForecastElement{
+							{Inferer: "worker0", Value: 0.042947662325033700},
+							{Inferer: "worker1", Value: 0.2816951641179930},
 						},
 					},
 				},
-				networkCombinedLoss: 0.01569376583279220,
-				epsilon:             1e-4,
-				pInferenceSynthesis: 2.0,
-				expected: map[string]*emissions.Inference{
-					"forecaster0": {Value: -0.036824032402771200},
-					"forecaster1": {Value: -0.025344200689386500},
-				},
-				expectedErr: nil,
 			},
-		*/
+			networkCombinedLoss: 0.01569376583279220,
+			epsilon:             1e-4,
+			pInferenceSynthesis: 2.0,
+			expected: map[string]*emissions.Inference{
+				"forecaster0": {Value: -0.036824032402771200},
+				"forecaster1": {Value: -0.025344200689386500},
+			},
+			expectedErr: nil,
+		},
 	}
 
 	for _, tc := range tests {
