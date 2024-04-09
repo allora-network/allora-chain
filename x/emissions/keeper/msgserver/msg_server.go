94ebcb34a3cf602efb44a56f40c16a05f6533f1d
Tyler <tyler@cyb3r.space>
2024-04-09 11:45:33 -0400

    test implementation of Dec message

--- x/emissions/keeper/msgserver/msg_server.go
+++ x/emissions/keeper/msgserver/msg_server.go
@@ -1,6 +1,9 @@
 package msgserver
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/allora-network/allora-chain/x/emissions/keeper"
 	"github.com/allora-network/allora-chain/x/emissions/types"
 )
@@ -14,4 +17,11 @@ var _ types.MsgServer = msgServer{}
 // NewMsgServerImpl returns an implementation of the module MsgServer interface.
 func NewMsgServerImpl(keeper keeper.Keeper) types.MsgServer {
 	return &msgServer{k: keeper}
+}
+
+func (k msgServer) TestDecMessage(ctx context.Context, msg *types.DecMessage) (*types.DecMessage, error) {
+	fmt.Println("TestDecMessage")
+	fmt.Println(msg.Value.String())
+	fmt.Println("")
+	return msg, nil
 }
