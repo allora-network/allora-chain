84c6e36152cfbb8153d22a23b4d908b0414d936a
br4e <milesminton@gmail.com>
2024-04-10 15:30:23 -0500

    Correct calc implied inferences (#124)

--- x/emissions/module/inference_synthesis/forecast_implied_inferences_test.go
+++ x/emissions/module/inference_synthesis/forecast_implied_inferences_test.go
@@ -118,36 +118,12 @@ func (s *InferenceSynthesisTestSuite) TestCalcForcastImpliedInferences() {
 			},
 			expectedErr: nil,
 		},
-		{ // ROW 1
-			name: "basic functionality, two workers, one forecaster",
-			inferenceByWorker: map[string]*emissions.Inference{
-				"worker0": {Value: -0.08069823482294600},
-				"worker1": {Value: 0.07295620166600340},
-			},
-			forecasts: &emissions.Forecasts{
-				Forecasts: []*emissions.Forecast{
-					{
-						Forecaster: "forecaster0",
-						ForecastElements: []*emissions.ForecastElement{
-							{Inferer: "worker0", Value: 0.0050411396925730900},
-							{Inferer: "worker1", Value: 3.31783420309334e-05},
-						},
-					},
-				},
-			},
-			networkCombinedLoss: 0.018593036157667700,
-			epsilon:             1e-4,
-			pInferenceSynthesis: 2.0,
-			expected: map[string]*emissions.Inference{
-				"forecaster0": {Value: 0.04164414697995540},
-			},
-			expectedErr: nil,
-		},
 		{ // ROW 2
-			name: "basic functionality 2, two workers, two forecasters",
+			name: "basic functionality 2, two workers, one forecaster",
 			inferenceByWorker: map[string]*emissions.Inference{
 				"worker0": {Value: -0.2797477698393250},
 				"worker1": {Value: 0.26856211587161100},
+				"worker2": {Value: 0.003934174100448460},
 			},
 			forecasts: &emissions.Forecasts{
 				Forecasts: []*emissions.Forecast{
@@ -156,23 +132,43 @@ func (s *InferenceSynthesisTestSuite) TestCalcForcastImpliedInferences() {
 						ForecastElements: []*emissions.ForecastElement{
 							{Inferer: "worker0", Value: 0.02089366880023640},
 							{Inferer: "worker1", Value: 0.3342267861383700},
-						},
-					},
-					{
-						Forecaster: "forecaster1",
-						ForecastElements: []*emissions.ForecastElement{
-							{Inferer: "worker0", Value: 0.042947662325033700},
-							{Inferer: "worker1", Value: 0.2816951641179930},
+							{Inferer: "worker2", Value: 0.0002604615062174660},
 						},
 					},
 				},
 			},
-			networkCombinedLoss: 0.01569376583279220,
+			networkCombinedLoss: 0.018593036157667700, // <- from Row 1
 			epsilon:             1e-4,
 			pInferenceSynthesis: 2.0,
 			expected: map[string]*emissions.Inference{
 				"forecaster0": {Value: -0.036824032402771200},
-				"forecaster1": {Value: -0.025344200689386500},
+			},
+			expectedErr: nil,
+		},
+		{ // ROW 3
+			name: "basic functionality 3, two workers, one forecaster",
+			inferenceByWorker: map[string]*emissions.Inference{
+				"worker0": {Value: -0.05142348924899710},
+				"worker1": {Value: -0.031653221198924200},
+				"worker2": {Value: -0.1018014248041400},
+			},
+			forecasts: &emissions.Forecasts{
+				Forecasts: []*emissions.Forecast{
+					{
+						Forecaster: "forecaster0",
+						ForecastElements: []*emissions.ForecastElement{
+							{Inferer: "worker0", Value: 0.00011708024633613200},
+							{Inferer: "worker1", Value: 0.013382222402411400},
+							{Inferer: "worker2", Value: 3.82471429104471e-05},
+						},
+					},
+				},
+			},
+			networkCombinedLoss: 0.01569376583279220, // <- from Row 2
+			epsilon:             1e-4,
+			pInferenceSynthesis: 2.0,
+			expected: map[string]*emissions.Inference{
+				"forecaster0": {Value: -0.07075177115182300},
 			},
 			expectedErr: nil,
 		},
