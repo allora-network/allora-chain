56c8748a894e7de9f27629072699fadcd0f82311
Michael <michael.c@upshot.xyz>
2024-04-12 10:13:52 -0500

    update proto

--- x/emissions/keeper/msgserver/msg_server_worker_payload.go
+++ x/emissions/keeper/msgserver/msg_server_worker_payload.go
@@ -2,13 +2,16 @@ package msgserver
 
 import (
 	"context"
-
 	"github.com/allora-network/allora-chain/x/emissions/types"
 )
 
 // A tx function that accepts a list of forecasts and possibly returns an error
 // Need to call this once per forecaster per topic inference solicitation round because protobuf does not nested repeated fields
 func (ms msgServer) InsertBulkWorkerPayload(ctx context.Context, msg *types.MsgInsertBulkWorkerPayload) (*types.MsgInsertBulkWorkerPayloadResponse, error) {
+
+	//senderPubKey := sims.NewPubKeyFromHex("311000040853706563756c6f73000b53706563756c6f734d4355000000000000")
+	//senderPubKey := sims.NewPubKeyFromHex(msg.Sender)
+	//log.Println("senderPubKey.VerifySignature()", senderPubKey.String())
 	// Check if the nonce is unfulfilled
 	nonceUnfulfilled, err := ms.k.IsWorkerNonceUnfulfilled(ctx, msg.TopicId, msg.Nonce)
 	if err != nil {
@@ -20,7 +23,6 @@ func (ms msgServer) InsertBulkWorkerPayload(ctx context.Context, msg *types.MsgI
 
 	for _, inference := range msg.Inferences {
 		// TODO check signatures! throw if invalid!
-
 		if inference.TopicId != msg.TopicId {
 			return nil, types.ErrInvalidTopicId
 		}
--- x/emissions/keeper/msgserver/msg_server_worker_payload_test.go
+++ x/emissions/keeper/msgserver/msg_server_worker_payload_test.go
@@ -1,8 +1,12 @@
 package msgserver_test
 
 import (
+	"bytes"
+	"fmt"
 	alloraMath "github.com/allora-network/allora-chain/math"
 	"github.com/allora-network/allora-chain/x/emissions/types"
+	"github.com/cosmos/cosmos-sdk/testutil/sims"
+	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -11,42 +15,48 @@ func (s *KeeperTestSuite) TestMsgInsertBulkWorkerPayload() {
 	require := s.Require()
 
 	// Mock setup for addresses
-	workerAddr := sdk.AccAddress(PKS[0].Address()).String()
+	//workerAddr := sdk.AccAddress(PKS[0].Address()).String()
 	inferencerAddr := sdk.AccAddress(PKS[0].Address()).String()
-	forecasterAddr1 := sdk.AccAddress(PKS[0].Address()).String()
-	forecasterAddr2 := sdk.AccAddress(PKS[0].Address()).String()
+	inferencerAddr1 := sdk.AccAddress(PKS[0].Address()).String()
+	inferencerAddr2 := sdk.AccAddress(PKS[0].Address()).String()
+	forecasterAddr := sdk.AccAddress(PKS[0].Address()).String()
 
+	test := sdk.AccAddress(simtestutil.CreateTestPubKeys(1)[0].Address())
 	// Create a MsgInsertBulkReputerPayload message
 	workerMsg := &types.MsgInsertBulkWorkerPayload{
-		Sender:  workerAddr,
+		Sender:  test.String(),
 		Nonce:   &types.Nonce{1},
 		TopicId: 1,
 		Inferences: []*types.Inference{
 			{
-				TopicId: 1,
-				Worker:  inferencerAddr,
-				Value:   alloraMath.NewDecFromInt64(100),
+				TopicId:   1,
+				Worker:    inferencerAddr,
+				Value:     alloraMath.NewDecFromInt64(100),
+				Signature: []byte("Inferences"),
 			},
 		},
 		Forecasts: []*types.Forecast{
 			{
 				TopicId:    1,
-				Forecaster: "",
+				Forecaster: forecasterAddr,
 				ForecastElements: []*types.ForecastElement{
 					{
-						Inferer: forecasterAddr1,
+						Inferer: inferencerAddr1,
 						Value:   alloraMath.NewDecFromInt64(200),
 					},
 					{
-						Inferer: forecasterAddr2,
+						Inferer: inferencerAddr2,
 						Value:   alloraMath.NewDecFromInt64(300),
 					},
 				},
+				Signature: []byte("Forecasts"),
 			},
 		},
-		Signature: []byte("Test"),
 	}
-
+	var buffer bytes.Buffer
+	buffer.WriteString(workerMsg.Sender)
+	te := sims.NewPubKeyFromHex(buffer.String())
+	fmt.Println("te", te.Address().String())
 	_, err := msgServer.InsertBulkWorkerPayload(ctx, workerMsg)
 	require.NoError(err, "InsertBulkWorkerPayload should not return an error")
 }
