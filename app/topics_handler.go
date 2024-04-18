e262598279403ea954cdc8ce7571f849b753cb6b
Diego C <xmariachi@gmail.com>
2024-04-18 17:30:12 +0200

    Diego/ora 1215 topichandler not updating state (#157)
    * Move change of state from topicsHandler to EndBlocker (otherwise it
    doesnt get persisted)
    * e2e worker full cycle tested (incl unfulfilled & fulfilling nonces)
    
    ---------
    
    Co-authored-by: br4e <milesminton@gmail.com>
    Co-authored-by: RedBird96 <73715579+RedBird96@users.noreply.github.com>

--- app/topics_handler.go
+++ app/topics_handler.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	emissionstypes "github.com/allora-network/allora-chain/x/emissions/types"
+	mintkeeper "github.com/allora-network/allora-chain/x/mint/keeper"
 
 	emissionskeeper "github.com/allora-network/allora-chain/x/emissions/keeper"
 	synth "github.com/allora-network/allora-chain/x/emissions/keeper/inference_synthesis"
@@ -13,14 +14,31 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+const secondsInAMonth uint64 = 2592000
+
 type TopicsHandler struct {
 	emissionsKeeper emissionskeeper.Keeper
+	mintKeeper      mintkeeper.Keeper
 }
 
-func NewTopicsHandler(emissionsKeeper emissionskeeper.Keeper) *TopicsHandler {
+func NewTopicsHandler(emissionsKeeper emissionskeeper.Keeper, mintKeeper mintkeeper.Keeper) *TopicsHandler {
 	return &TopicsHandler{
 		emissionsKeeper: emissionsKeeper,
+		mintKeeper:      mintKeeper,
 	}
+}
+
+func (th *TopicsHandler) calculatePreviousBlockApproxTime(ctx sdk.Context, blockDifference int64) (uint64, error) {
+	mintParams, err := th.mintKeeper.GetParams(ctx)
+	if err != nil {
+		fmt.Println("Error getting mint params: ", err)
+		return 0, err
+	}
+	BlocksPerMonth := mintParams.GetBlocksPerMonth()
+	var approximateTimePerBlockSeconds float64 = float64(secondsInAMonth) / float64(BlocksPerMonth)
+	var diffFloat = (float64(blockDifference) * approximateTimePerBlockSeconds)
+	var previousBlockApproxTime = uint64(ctx.BlockTime().Unix() - int64(diffFloat))
+	return previousBlockApproxTime, nil
 }
 
 func (th *TopicsHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
@@ -43,34 +61,79 @@ func (th *TopicsHandler) PrepareProposalHandler() sdk.PrepareProposalHandler {
 			wg.Add(1)
 			go func(topic *emissionstypes.Topic) {
 				defer wg.Done()
-				// Get previous topic height to repute
-				previousBlockHeight := topic.EpochLastEnded
-				if previousBlockHeight <= 0 {
-					fmt.Println("Previous block height is less than or equal to 0, skipping")
-					return
-				} else {
-					fmt.Println("Current block height: ", currentBlockHeight, "Previous block height: ", previousBlockHeight)
-				}
+
 				// Check if the inference and loss cadence is met, then run inf and loss generation
-				if currentBlockHeight >= topic.EpochLastEnded+topic.EpochLength {
+				if currentBlockHeight == topic.EpochLastEnded+topic.EpochLength ||
+					currentBlockHeight-topic.EpochLastEnded > 2*topic.EpochLength {
+					// WORKER
 					fmt.Printf("Triggering inference generation for topic: %v metadata: %s default arg: %s. \n",
 						topic.Id, topic.Metadata, topic.DefaultArg)
-					go generateInferences(topic.InferenceLogic, topic.InferenceMethod, topic.DefaultArg, topic.Id, currentNonce)
-					th.emissionsKeeper.AddWorkerNonce(ctx, topic.Id, &currentNonce)
 
-					fmt.Printf("Triggering Losses cadence met for topic: %v metadata: %s default arg: %s \n",
-						topic.Id, topic.Metadata, topic.DefaultArg)
-					// We don't want just the latest inferences but the ValueBundle (I_i) instead
-					currentTime := uint64(ctx.BlockTime().Unix())
-					// Get from previous blockHeight
-					inferences, _, err := synth.GetNetworkInferencesAtBlock(ctx, th.emissionsKeeper, topic.Id, previousBlockHeight)
-					fmt.Println("Error getting latest inferences: ", err)
+					workerNonces, err := th.emissionsKeeper.GetUnfulfilledWorkerNonces(ctx, topic.Id)
 					if err != nil {
+						fmt.Println("Error getting worker nonces: ", err)
 						return
 					}
-					go generateLosses(inferences, topic.LossLogic, topic.LossMethod, topic.Id, currentNonce, currentTime)
-					previousNonce := emissionstypes.Nonce{BlockHeight: previousBlockHeight}
-					th.emissionsKeeper.AddReputerNonce(ctx, topic.Id, &currentNonce, &previousNonce)
+					// iterate over all the worker nonces to find if this is unfulfilled
+					for _, nonce := range workerNonces.Nonces {
+						if nonce.BlockHeight == currentBlockHeight {
+							fmt.Println("Current Worker block height has been found unfulfilled, requesting inferences ", currentNonce)
+							go generateInferences(topic.InferenceLogic, topic.InferenceMethod, topic.DefaultArg, topic.Id, currentNonce)
+						}
+					}
+
+					// REPUTER
+					// Get previous topic height to repute
+					previousBlockHeight := topic.EpochLastEnded
+					if previousBlockHeight < 0 {
+						fmt.Println("Previous block height is less than 0, skipping")
+						return
+					}
+					previousToPreviousBlockHeight := previousBlockHeight - topic.EpochLength
+					if previousBlockHeight < 0 {
+						fmt.Println("Previous to previous block height is less than 0, skipping")
+						return
+					} else {
+						fmt.Println("Previous block height: ", previousBlockHeight, "Previous to previous block height: ", previousToPreviousBlockHeight)
+					}
+					fmt.Printf("Triggering Losses cadence met for topic: %v metadata: %s default arg: %s \n",
+						topic.Id, topic.Metadata, topic.DefaultArg)
+					reputerNonces, err := th.emissionsKeeper.GetUnfulfilledReputerNonces(ctx, topic.Id)
+					if err != nil {
+						fmt.Println("Error getting reputer nonces: ", err)
+						return
+					}
+
+					// iterate over all the reputer nonces to find if this is unfulfilled
+					for _, nonce := range reputerNonces.Nonces {
+						if nonce.ReputerNonce.BlockHeight == previousBlockHeight &&
+							nonce.WorkerNonce.BlockHeight == previousToPreviousBlockHeight {
+							fmt.Println("Current Reputer block height has been found unfulfilled, requesting reputers for block ", previousBlockHeight)
+							reputerValueBundle, inferencesBlockHeight, err := synth.GetNetworkInferencesAtBlock(ctx, th.emissionsKeeper, topic.Id, previousBlockHeight)
+							if err != nil {
+								fmt.Println("Error getting latest inferences at block: ", previousBlockHeight, ", error: ", err)
+								continue
+							}
+							if reputerValueBundle == nil {
+								fmt.Println("Reputer value bundle is nil, skipping")
+								continue
+							}
+							blockDifference := currentBlockHeight - inferencesBlockHeight
+							previousBlockApproxTime, err := th.calculatePreviousBlockApproxTime(ctx, blockDifference)
+							if err != nil {
+								fmt.Println("Error calculating previous block approx time: ", err)
+								continue
+							}
+							// Get approximated time of the previous block
+							reputerNonce := emissionstypes.Nonce{BlockHeight: previousBlockHeight}
+							workerNonce := emissionstypes.Nonce{BlockHeight: previousToPreviousBlockHeight}
+							// print the request of loss generation
+							fmt.Println("Requesting losses for topic: ", topic.Id, "reputer nonce: ", reputerNonce, "worker nonce: ", workerNonce, "previous block approx time: ", previousBlockApproxTime)
+							go generateLosses(reputerValueBundle, topic.LossLogic, topic.LossMethod, topic.Id, reputerNonce, workerNonce, previousBlockApproxTime)
+						} else {
+							fmt.Println("Reputer nonce not met: (", nonce.ReputerNonce.BlockHeight, ",", nonce.WorkerNonce.BlockHeight, ") for topic: ", topic.Id, "block height: ", currentBlockHeight, "epoch length: ", topic.EpochLength)
+						}
+					}
 				} else {
 					fmt.Println("Inference and Losses cadence not met for topic: ", topic.Id, "block height: ", currentBlockHeight, "epoch length: ", topic.EpochLength, "last ended: ", topic.EpochLastEnded)
 				}
