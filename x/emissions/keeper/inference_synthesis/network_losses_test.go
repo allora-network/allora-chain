9730850500bbd1aaa8e0df1f8ba8214cff54fd26
br4e <milesminton@gmail.com>
2024-04-22 15:20:10 -0700

    dummy TestCalcNetworkLosses

--- x/emissions/keeper/inference_synthesis/network_losses_test.go
+++ x/emissions/keeper/inference_synthesis/network_losses_test.go
@@ -1,7 +1,11 @@
 package inference_synthesis_test
 
 import (
+	"testing"
+
+	cosmosMath "cosmossdk.io/math"
 	alloraMath "github.com/allora-network/allora-chain/math"
+	"github.com/stretchr/testify/require"
 
 	"github.com/allora-network/allora-chain/x/emissions/keeper/inference_synthesis"
 	emissions "github.com/allora-network/allora-chain/x/emissions/types"
@@ -51,6 +55,112 @@ func (s *InferenceSynthesisTestSuite) TestRunningWeightedAvgUpdate() {
 				s.Require().NoError(err, "No error expected but got one")
 				s.Require().True(alloraMath.InDelta(tc.expectedLoss.Loss, updatedLoss.Loss, alloraMath.MustNewDecFromString("0.00001")), "Loss should match the expected value within epsilon")
 				s.Require().Equal(tc.expectedLoss.SumWeight, updatedLoss.SumWeight, "Sum of weights should match the expected value")
+			}
+		})
+	}
+}
+
+func TestCalcNetworkLosses(t *testing.T) {
+	tests := []struct {
+		name            string
+		stakesByReputer map[inference_synthesis.Worker]cosmosMath.Uint
+		reportedLosses  emissions.ReputerValueBundles
+		epsilon         alloraMath.Dec
+		expectedOutput  emissions.ValueBundle
+		expectedError   error
+	}{
+		{
+			name: "single reputer single inferer",
+			stakesByReputer: map[inference_synthesis.Worker]cosmosMath.Uint{
+				"worker0": cosmosMath.NewUintFromString("1"),
+			},
+			reportedLosses: emissions.ReputerValueBundles{
+				ReputerValueBundles: []*emissions.ReputerValueBundle{
+					{
+						ValueBundle: &emissions.ValueBundle{
+							Reputer:       "worker0",
+							CombinedValue: alloraMath.MustNewDecFromString("0.5"),
+							InfererValues: []*emissions.WorkerAttributedValue{
+								{Worker: "worker0", Value: alloraMath.MustNewDecFromString("0.5")},
+							},
+						},
+					},
+				},
+			},
+			epsilon: alloraMath.MustNewDecFromString("1e-4"),
+			expectedOutput: emissions.ValueBundle{
+				CombinedValue: alloraMath.MustNewDecFromString("1.648721"), // e^0.5
+				InfererValues: []*emissions.WorkerAttributedValue{
+					{Worker: "worker1", Value: alloraMath.MustNewDecFromString("1.648721")},
+				},
+			},
+			expectedError: nil,
+		},
+		/*
+			{
+				name: "multiple reputers multiple values",
+				stakesByReputer: map[inference_synthesis.Worker]cosmosMath.Uint{
+					"worker1": cosmosMath.NewUintFromString("1"),
+					"worker2": cosmosMath.NewUintFromString("2"),
+				},
+				reportedLosses: emissions.ReputerValueBundles{
+					ReputerValueBundles: []*emissions.ReputerValueBundle{
+						{
+							ValueBundle: &emissions.ValueBundle{
+								Reputer:       "worker1",
+								CombinedValue: alloraMath.MustNewDecFromString("0.5"),
+								InfererValues: []*emissions.WorkerAttributedValue{
+									{Worker: "worker1", Value: alloraMath.MustNewDecFromString("0.5")},
+								},
+							},
+						},
+						{
+							ValueBundle: &emissions.ValueBundle{
+								Reputer:       "worker2",
+								CombinedValue: alloraMath.MustNewDecFromString("0.2"),
+								InfererValues: []*emissions.WorkerAttributedValue{
+									{Worker: "worker1", Value: alloraMath.MustNewDecFromString("0.3")},
+									{Worker: "worker2", Value: alloraMath.MustNewDecFromString("0.2")},
+								},
+							},
+						},
+					},
+				},
+				epsilon: alloraMath.MustNewDecFromString("1e-4"),
+				expectedOutput: emissions.ValueBundle{
+					CombinedValue: alloraMath.MustNewDecFromString("1.412538"), // Calculated e^(weighted average of 0.5 and 0.2)
+					InfererValues: []*emissions.WorkerAttributedValue{
+						{Worker: "worker1", Value: alloraMath.MustNewDecFromString("1.481689")},
+						{Worker: "worker2", Value: alloraMath.MustNewDecFromString("1.221403")},
+					},
+				},
+				expectedError: nil,
+			},
+		*/
+		/*
+			{
+				name: "error handling invalid decimal",
+				stakesByReputer: map[inference_synthesis.Worker]cosmosMath.Uint{
+					"worker1": cosmosMath.NewUintFromString("NaN"),
+				},
+				reportedLosses: emissions.ReputerValueBundles{},
+				epsilon:        alloraMath.MustNewDecFromString("1e-4"),
+				expectedOutput: emissions.ValueBundle{},
+				expectedError:  fmt.Errorf("invalid decimal value"),
+			},
+		*/
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			output, err := inference_synthesis.CalcNetworkLosses(tc.stakesByReputer, tc.reportedLosses, tc.epsilon)
+			if tc.expectedError != nil {
+				require.Error(t, err)
+				require.EqualError(t, err, tc.expectedError.Error())
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedOutput.CombinedValue, output.CombinedValue)
+				require.ElementsMatch(t, tc.expectedOutput.InfererValues, output.InfererValues)
 			}
 		})
 	}
