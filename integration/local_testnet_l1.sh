daceacd953948118bdaec887d3ab87b5a2ca3d17
Kenny P <17100641+kpeluso@users.noreply.github.com>
2024-05-08 16:10:38 -0400

    try to get tests to work for m1 setup; add update-param shim to test … (#224)
    …large number log10
    
    ---------
    
    Co-authored-by: vladupshot <154623109+vladupshot@users.noreply.github.com>
    Co-authored-by: Tobi Okedeji <tobiokedeji@gmail.com>

--- integration/local_testnet_l1.sh
+++ integration/local_testnet_l1.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
-set -eu
+set -eu  #e
 
 # Ensure we're in integration folder
 cd "$(dirname "$0")"
 
+ALLORAD_BIN=$(which allorad)
+
 DOCKER_IMAGE=allorad
-VALIDATOR_NUMBER=3
+VALIDATOR_NUMBER="${VALIDATOR_NUMBER:-3}"
 VALIDATOR_PREFIX=validator
-NETWORK_PREFIX="172.20.0"
+NETWORK_PREFIX="192.168.250"
 VALIDATORS_IP_START=10
+VALIDATORS_RPC_PORT_START=26657
 HEADS_IP_START=20
 CHAIN_ID="${CHAIN_ID:-devnet}"
-LOCALNET_DATADIR="./$CHAIN_ID"
+LOCALNET_DATADIR="$(pwd)/$CHAIN_ID"
 
 ACCOUNTS_TOKENS=1000000
 
 ENV_L1="${LOCALNET_DATADIR}/.env"
-L1_COMPOSE="compose_l1.yaml"
+L1_COMPOSE=${LOCALNET_DATADIR}/compose_l1.yaml
 
+if [ -d "$LOCALNET_DATADIR" ]; then
+    echo "Folder $LOCALNET_DATADIR already exist, need to delete it before running the script."
+    read -p "Stop validators and Delete $LOCALNET_DATADIR folder??[y/N] " -n 1 -r
+    echo    # (optional) move to a new line
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # Stop if containers are already up
+        [ $(docker ps |wc -l) -gt 1 ]  && docker compose -f $L1_COMPOSE down
+        rm -rf $LOCALNET_DATADIR
+    fi
+fi
 mkdir -p $LOCALNET_DATADIR
-echo "UID_GID=$(id -u):$(id -g)" > ${ENV_L1}
-echo "NETWORK_PREFIX=$NETWORK_PREFIX" >> ${ENV_L1}
+
+UID_GID="$(id -u):$(id -g)"
+# echo "NETWORK_PREFIX=$NETWORK_PREFIX" >> ${ENV_L1}
 echo "CHAIN_ID=$CHAIN_ID" >> ${ENV_L1}
-echo "ALLORA_RPC=http://${NETWORK_PREFIX}.10:26657" >> ${ENV_L1}  # Take validator0
+echo "ALLORA_RPC=http://${NETWORK_PREFIX}.${VALIDATORS_IP_START}:26657" >> ${ENV_L1}  # Take validator0
 
 echo "Build the docker image"
-docker build -t $DOCKER_IMAGE -f ../Dockerfile.development ..
+pushd ..
+docker build -t $DOCKER_IMAGE -f ./Dockerfile.development .
+popd
 
 echo "Download generate_genesis.sh from testnet"
 mkdir -p ${LOCALNET_DATADIR}
@@ -45,8 +61,9 @@ docker run \
     -v ${LOCALNET_DATADIR}:/data \
     -e COMMON_HOME_DIR=/data \
     -e HOME=/data \
+    -e VALIDATOR_NUMBER=$VALIDATOR_NUMBER \
     --entrypoint=/data/generate_genesis.sh \
-    $DOCKER_IMAGE
+    $DOCKER_IMAGE > /dev/null 2>&1
 
 echo "Whitelist faucet account"
 FAUCET_ADDRESS=$(docker run -t \
@@ -83,27 +100,40 @@ for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
 done
 
 echo "PEERS=$PEERS" >> ${ENV_L1}
+echo "Generate docker compose file"
+NETWORK_PREFIX=$NETWORK_PREFIX envsubst < compose_l1_header.yaml > $L1_COMPOSE
+for ((i=0; i<$VALIDATOR_NUMBER; i++)); do
+    ipAddress="${NETWORK_PREFIX}.$((VALIDATORS_IP_START+i))" \
+    moniker="${VALIDATOR_PREFIX}${i}" \
+    validatorPort=$((VALIDATORS_RPC_PORT_START+i)) \
+    PEERS=$PEERS \
+    NETWORK_PREFIX=$NETWORK_PREFIX \
+    LOCALNET_DATADIR=$LOCALNET_DATADIR \
+    UID_GID=$UID_GID \
+    envsubst < validator.tmpl >> $L1_COMPOSE
+done
 
 echo "Launching the network"
-docker compose --env-file ${ENV_L1} -f $L1_COMPOSE up -d
+docker compose -f $L1_COMPOSE up -d
 
 echo "Waiting validator is up"
 curl -o /dev/null --connect-timeout 5 \
     --retry 10 \
     --retry-delay 10 \
     --retry-all-errors \
-    http://${NETWORK_PREFIX}.${VALIDATORS_IP_START}:26657/status
+    http://localhost:$VALIDATORS_RPC_PORT_START/status
 
 echo "Checking the network is up and running"
 heights=()
 validators=()
 for ((v=0; v<$VALIDATOR_NUMBER; v++)); do
-    height=$(curl -s http://${NETWORK_PREFIX}.$((VALIDATORS_IP_START+v)):26657/status|jq -r .result.sync_info.latest_block_height)
+    height=$(curl -s http://localhost:$((VALIDATORS_RPC_PORT_START+v))/status|jq -r .result.sync_info.latest_block_height)
     heights+=($height)
-    echo "Got height: ${heights[$v]} from validator: ${NETWORK_PREFIX}.$((VALIDATORS_IP_START+v))"
-    validators+=("${NETWORK_PREFIX}.$((VALIDATORS_IP_START+v))")
+    echo "Got height: ${heights[$v]} from validator: http://localhost:$((VALIDATORS_RPC_PORT_START+v))"
+    validators+=("localhost:$((VALIDATORS_RPC_PORT_START+v))")
     sleep 5
 done
+
 echo "Populate validators.json with validators addresses"
 jq --compact-output --null-input '$ARGS.positional' --args -- "${validators[@]}" > ${LOCALNET_DATADIR}/validators.json
 
@@ -116,8 +146,20 @@ if [ ${#heights[@]} -eq $VALIDATOR_NUMBER ]; then
     done
 fi
 
+# Configure client.toml
+$ALLORAD_BIN --home $LOCALNET_DATADIR/genesis config set client keyring-backend test
+
 if [ $chain_status -eq $((VALIDATOR_NUMBER-1)) ]; then
     echo "Chain is up and running"
+    echo
+    echo "Some usefull commands:"
+    echo "  - 'docker compose -f $L1_COMPOSE logs -f' -- To see logs of the containers"
+    echo "  - 'docker compose -f $L1_COMPOSE logs -f validator[0-...]' -- To see logs of the specified validator"
+    echo "  - 'docker compose -f $L1_COMPOSE down' -- To stop all the validators"
+    echo "  - http://localhost:2665[7-...] -- Validators RPC address, port = 26657 + VALIDATOR_NUMBER"
+    echo "  -   - 'curl http://localhost:26658/status|jq .' -- To get validator1 (26657+1=26658) RPC address"
+    echo "To use allorad commands, you can specify \'$LOCALNET_DATADIR/genesis\' as --home, eg.:"
+    echo "  - 'allorad --home $LOCALNET_DATADIR/genesis status'"
 else
     echo "Chain is not producing blocks"
     echo "If run localy you can check the logs with: docker logs allorad_validator_0"
