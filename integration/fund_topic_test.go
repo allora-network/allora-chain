2fddc4868d65bdde2a0d4f32eb36b9c474c03453
vladupshot <154623109+vladupshot@users.noreply.github.com>
2024-05-10 11:49:21 +1200

    Adjust  scripts to spawn local allora network on macs  (#225)
    Adjust local_l1 script to be able to spawn local allora network on macs
    
    ---------
    
    Signed-off-by: Kenny P <17100641+kpeluso@users.noreply.github.com>
    Co-authored-by: Tyler <tyler@cyb3r.space>
    Co-authored-by: Kenny P <17100641+kpeluso@users.noreply.github.com>
    Co-authored-by: Kenny <pelusoken@gmail.com>
    Co-authored-by: Tobi Okedeji <tobiokedeji@gmail.com>

---
+++ integration/fund_topic_test.go
@@ -0,0 +1,49 @@
+package integration_test
+
+import (
+	cosmosMath "cosmossdk.io/math"
+	emissionstypes "github.com/allora-network/allora-chain/x/emissions/types"
+	"github.com/stretchr/testify/require"
+)
+
+func FundTopic1(m TestMetadata) {
+	txResp, err := m.n.Client.BroadcastTx(
+		m.ctx,
+		m.n.BobAcc,
+		&emissionstypes.MsgFundTopic{
+			Sender:  m.n.BobAddr,
+			TopicId: 1,
+			Amount:  cosmosMath.NewInt(10000),
+		},
+	)
+	require.NoError(m.t, err)
+	_, err = m.n.Client.WaitForTx(m.ctx, txResp.TxHash)
+	require.NoError(m.t, err)
+	resp := &emissionstypes.MsgFundTopicResponse{}
+	err = txResp.Decode(resp)
+	require.NoError(m.t, err)
+}
+
+func CheckTopic1Activated(m TestMetadata) {
+	// Fetch only active topics
+	pagi := &emissionstypes.QueryActiveTopicsRequest{
+		Pagination: &emissionstypes.SimpleCursorPaginationRequest{
+			Limit: 10,
+		},
+	}
+	activeTopics, err := m.n.QueryEmissions.GetActiveTopics(
+		m.ctx,
+		pagi)
+	require.NoError(m.t, err, "Fetching active topics should not produce an error")
+
+	// Verify the correct number of active topics is retrieved
+	require.Equal(m.t, len(activeTopics.Topics), 1, "Should retrieve exactly one active topics")
+}
+
+// Must come after a reputer is registered and staked in topic 1
+func TopicFundingChecks(m TestMetadata) {
+	m.t.Log("--- Check funding Topic 1 ---")
+	FundTopic1(m)
+	m.t.Log("--- Check reactivating Topic 1 ---")
+	CheckTopic1Activated(m)
+}
