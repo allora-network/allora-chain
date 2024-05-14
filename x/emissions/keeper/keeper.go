allora-network/allora-chain

commit 208c8a7528dc4aa959973e5ac959fcac58c47ee9

Add rewards to staking module  (#214)

x/emissions/module/rewards/rewards.go
@@ -444,6 +444,8 @@ func payoutRewards(ctx sdk.Context, k keeper.Keeper, rewards []TaskRewards) erro
 		rewardInt := reward.Reward.Abs().SdkIntTrim()
 
 		if reward.Type == ReputerRewardType {
+			coins := sdk.NewCoins(sdk.NewCoin(params.DefaultBondDenom, rewardInt))
+			k.SendCoinsFromAccountToModule(ctx, reward.Address, types.AlloraStakingAccountName, coins)
 			k.AddStake(ctx, reward.TopicId, reward.Address, cosmosMath.Uint(rewardInt))
 		} else {
 			err = k.BankKeeper().SendCoinsFromModuleToAccount(

commit dbbd100a0b2bb6f02bcdd346b5dbf5b11d3e8bd3

Fix bug introduced in #214 where rather than paying rewards, we steal funds from reputers

x/emissions/keeper/keeper.go
@@ -2061,14 +2061,19 @@ func (k *Keeper) BankKeeper() BankKeeper {
 	return k.bankKeeper
 }
 
-// SendCoinsFromModuleToAccount
+// wrapper around bank keeper SendCoinsFromModuleToAccount
 func (k *Keeper) SendCoinsFromModuleToAccount(ctx context.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error {
 	return k.bankKeeper.SendCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, amt)
 }
 
-// SendCoinsFromAccountToModule
+// wrapper around bank keeper SendCoinsFromAccountToModule
 func (k *Keeper) SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
 	return k.bankKeeper.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, amt)
+}
+
+// wrapper around bank keeper SendCoinsFromModuleToModule
+func (k *Keeper) SendCoinsFromModuleToModule(ctx context.Context, senderModule, recipientModule string, amt sdk.Coins) error {
+	return k.bankKeeper.SendCoinsFromModuleToModule(ctx, senderModule, recipientModule, amt)
 }
 
 // GetTotalRewardToDistribute

x/emissions/module/rewards/reputer_rewards.go
@@ -204,7 +204,7 @@ func GetRewardForReputerFromTotalReward(
 			if err != nil {
 				return nil, err
 			}
-			err = keeper.BankKeeper().SendCoinsFromModuleToModule(
+			err = keeper.SendCoinsFromModuleToModule(
 				ctx,
 				types.AlloraRewardsAccountName,
 				types.AlloraPendingRewardForDelegatorAccountName,

x/emissions/module/rewards/rewards.go
@@ -143,7 +143,7 @@ func GenerateRewardsDistributionByTopic(
 	// Send remaining collected inference request fees to the Ecosystem module account
 	// They will be paid out to reputers, workers, and validators
 	// according to the formulas in the beginblock of the mint module
-	err = k.BankKeeper().SendCoinsFromModuleToModule(
+	err = k.SendCoinsFromModuleToModule(
 		ctx,
 		types.AlloraRequestsAccountName,
 		mintTypes.EcosystemModuleName,
@@ -496,16 +496,15 @@ func payoutRewards(
 		}
 
 		rewardInt := reward.Reward.Abs().SdkIntTrim()
+		coins := sdk.NewCoins(sdk.NewCoin(params.DefaultBondDenom, rewardInt))
 
 		if reward.Type == ReputerRewardType {
-			coins := sdk.NewCoins(sdk.NewCoin(params.DefaultBondDenom, rewardInt))
-			err = k.SendCoinsFromAccountToModule(ctx, reward.Address, types.AlloraStakingAccountName, coins)
+			err = k.SendCoinsFromModuleToModule(ctx, types.AlloraRewardsAccountName, types.AlloraStakingAccountName, coins)
 			if err != nil {
 				ret = append(ret, errors.Wrapf(
 					err,
-					"failed to send coins from account to rewards module %s -> %s",
-					reward.Address.String(),
-					types.AlloraStakingAccountName,
+					"failed to send coins from rewards module to staking module: %s",
+					coins.String(),
 				))
 				continue
 			}
@@ -519,7 +518,7 @@ func payoutRewards(
 				ctx,
 				types.AlloraRewardsAccountName,
 				address,
-				sdk.NewCoins(sdk.NewCoin(params.DefaultBondDenom, rewardInt)),
+				coins,
 			)
 			if err != nil {
 				ret = append(ret, errors.Wrapf(

x/emissions/module/rewards/rewards_test.go
@@ -588,6 +588,7 @@ func (s *RewardsTestSuite) TestRewardsIncreasesBalance() {
 	block := int64(600)
 	s.ctx = s.ctx.WithBlockHeight(block)
 	epochLength := int64(10800)
+	s.MintTokensToModule(types.AlloraStakingAccountName, cosmosMath.NewInt(10000000000))
 
 	// Reputer Addresses
 	reputerAddrs := []sdk.AccAddress{
@@ -677,14 +678,12 @@ func (s *RewardsTestSuite) TestRewardsIncreasesBalance() {
 		s.Require().NoError(err)
 	}
 
-	var initialStake int64 = 1000
-	initialStakeCoins := sdk.NewCoins(sdk.NewCoin(params.DefaultBondDenom, cosmosMath.NewInt(initialStake)))
-	s.bankKeeper.MintCoins(s.ctx, types.AlloraStakingAccountName, initialStakeCoins)
-	s.bankKeeper.SendCoinsFromModuleToAccount(s.ctx, types.AlloraStakingAccountName, reputerAddrs[0], initialStakeCoins)
+	initialStake := cosmosMath.NewInt(1000)
+	s.MintTokensToAddress(reputerAddrs[0], initialStake)
 	fundTopicMessage := types.MsgFundTopic{
 		Sender:  reputerAddrs[0].String(),
 		TopicId: topicId,
-		Amount:  cosmosMath.NewInt(initialStake),
+		Amount:  initialStake,
 	}
 	_, err = s.msgServer.FundTopic(s.ctx, &fundTopicMessage)
 	s.Require().NoError(err)