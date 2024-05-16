a6401e3faea35c1f4c567caa100ba908ae201569
Kenny <pelusoken@gmail.com>
2024-05-16 19:04:03 -0400

    ensureWorkerPresence is needed => rm nondeterminism from range over maps

--- x/emissions/module/rewards/common.go
+++ x/emissions/module/rewards/common.go
@@ -12,3 +12,12 @@ func GetSortedUint64Keys[T any](m map[uint64]T) []uint64 {
 	sort.Slice(keys, func(i, j int) bool { return keys[i] < keys[j] })
 	return keys
 }
+
+func GetSortedStringKeys[T any](m map[string]T) []string {
+	keys := make([]string, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	return keys
+}
