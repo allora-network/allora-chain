67e6a5e377407fea6087fbb95b6865d65fd84fc8
br4e <milesminton@gmail.com>
2024-05-24 10:01:20 -0700

    Update msg_server_stake_test.go

--- x/emissions/keeper/msgserver/msg_server_stake_test.go
+++ x/emissions/keeper/msgserver/msg_server_stake_test.go
@@ -364,7 +364,7 @@ func (s *KeeperTestSuite) TestReputerCantSelfDelegateStake() {
 	keeper := s.emissionsKeeper
 
 	delegatorAddr := sdk.AccAddress(PKS[1].Address())
-	reputerAddr := sdk.AccAddress(PKS[1].Address())
+	reputerAddr := delegatorAddr
 	topicId := uint64(123)
 	stakeAmount := cosmosMath.NewInt(50)
 	s.MintTokensToAddress(delegatorAddr, cosmosMath.NewInt(1000))
