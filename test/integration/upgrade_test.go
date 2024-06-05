ac7d510f35726004a9f25bd0f99e740e0c599cc1
Tyler <tyler@cyb3r.space>
2024-06-05 12:51:10 -0400

    Software upgrade should be a msg in the governance proposal

--- test/integration/upgrade_test.go
+++ test/integration/upgrade_test.go
@@ -1,6 +1,8 @@
 package integration_test
 
 import (
+	"fmt"
+
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	testCommon "github.com/allora-network/allora-chain/test/common"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
@@ -17,6 +19,7 @@ func getDepositRequired(m testCommon.TestConfig) sdktypes.Coin {
 
 func proposeUpgrade(m testCommon.TestConfig) uint64 {
 	name := "vIntegration"
+	summary := "Upgrade to vIntegration software version"
 	msgSoftwareUpgrade := &upgradetypes.MsgSoftwareUpgrade{
 		Authority: authtypes.NewModuleAddress("gov").String(),
 		Plan: upgradetypes.Plan{
@@ -25,41 +28,28 @@ func proposeUpgrade(m testCommon.TestConfig) uint64 {
 			Info:   "{}",
 		},
 	}
-	txResp, err := m.Client.BroadcastTx(m.Ctx, m.AliceAcc, msgSoftwareUpgrade)
+	submitProposalMsg := &govtypesv1.MsgSubmitProposal{
+		Title:    name,
+		Summary:  summary,
+		Proposer: m.AliceAddr,
+		Metadata: fmt.Sprintf(
+			"{title:\"%s\",summary:\"%s\"}", name, summary,
+		), // metadata must match title and summary exactly
+		Expedited: true,
+		InitialDeposit: sdktypes.NewCoins(
+			getDepositRequired(m),
+		),
+	}
+	submitProposalMsg.SetMsgs([]sdktypes.Msg{msgSoftwareUpgrade})
+	txResp, err := m.Client.BroadcastTx(m.Ctx, m.AliceAcc, submitProposalMsg)
 	require.NoError(m.T, err)
 	_, err = m.Client.WaitForTx(m.Ctx, txResp.TxHash)
 	require.NoError(m.T, err)
-	msgSoftwareUpgradeResponse := &upgradetypes.MsgSoftwareUpgradeResponse{}
-	err = txResp.Decode(msgSoftwareUpgradeResponse)
+	submitProposalMsgResponse := &govtypesv1.MsgSubmitProposalResponse{}
+	err = txResp.Decode(submitProposalMsgResponse)
 	require.NoError(m.T, err)
-	require.NotNil(m.T, msgSoftwareUpgradeResponse)
-	return 3
-
-	// here's how to do a vanilla governance proposal - still not sure
-	// if we have to propose this by hand ourselves or use the upgrade
-	// module to do it for us
-	//
-	//	submitProposalMsg := &govtypesv1.MsgSubmitProposal{
-	//		Title:    title,
-	//		Summary:  summary,
-	//		Proposer: m.AliceAddr,
-	//		Metadata: fmt.Sprintf(
-	//			"{title:\"%s\",summary:\"%s\"}", title, summary,
-	//		), // metadata must match title and summary exactly
-	//		Expedited: true,
-	//		InitialDeposit: sdktypes.NewCoins(
-	//			getDepositRequired(m),
-	//		),
-	//	}
-	//	txResp, err := m.Client.BroadcastTx(m.Ctx, m.AliceAcc, submitProposalMsg)
-	//	require.NoError(m.T, err)
-	//	_, err = m.Client.WaitForTx(m.Ctx, txResp.TxHash)
-	//	require.NoError(m.T, err)
-	//	submitProposalMsgResponse := &govtypesv1.MsgSubmitProposalResponse{}
-	//	err = txResp.Decode(submitProposalMsgResponse)
-	//	require.NoError(m.T, err)
-	//	require.NotNil(m.T, submitProposalMsgResponse.ProposalId)
-	//	return submitProposalMsgResponse.ProposalId
+	require.NotNil(m.T, submitProposalMsgResponse.ProposalId)
+	return submitProposalMsgResponse.ProposalId
 }
 
 func UpgradeChecks(m testCommon.TestConfig) {
