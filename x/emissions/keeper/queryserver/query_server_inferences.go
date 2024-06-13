f96759259aac571371a9a4ff62d0dda7f347d11e
br4e <milesminton@gmail.com>
2024-06-13 09:37:20 -0700

    Create GetLatestNetworkInference query function

--- x/emissions/keeper/queryserver/query_server_inferences.go
+++ x/emissions/keeper/queryserver/query_server_inferences.go
@@ -61,3 +61,32 @@ func (qs queryServer) GetNetworkInferencesAtBlock(ctx context.Context, req *type
 
 	return &types.QueryNetworkInferencesAtBlockResponse{NetworkInferences: networkInferences}, nil
 }
+
+// Return full set of inferences in I_i from the chain
+func (qs queryServer) GetLatestNetworkInferences(ctx context.Context, req *types.QueryNetworkInferencesAtBlockRequest) (*types.QueryNetworkInferencesAtBlockResponse, error) {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	topic, err := qs.k.GetTopic(ctx, req.TopicId)
+	if err != nil {
+		return nil, status.Errorf(codes.NotFound, "topic %v not found", req.TopicId)
+	}
+	if topic.EpochLastEnded == 0 {
+		return nil, status.Errorf(codes.NotFound, "network inference not available for topic %v", req.TopicId)
+	}
+	if req.BlockHeightLastInference > sdkCtx.BlockHeight() {
+		return nil, status.Errorf(codes.InvalidArgument, "block height cannot be greater than current block height %v", sdkCtx.BlockHeight())
+	}
+
+	networkInferences, err := synth.GetNetworkInferencesAtBlock(
+		sdk.UnwrapSDKContext(ctx),
+		qs.k,
+		req.TopicId,
+		req.BlockHeightLastInference,
+		req.BlockHeightLastReward,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryNetworkInferencesAtBlockResponse{NetworkInferences: networkInferences}, nil
+}
