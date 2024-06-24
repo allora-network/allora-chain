d84344e60429ab6b73066fba25649e828dc6792e
Tyler <tyler@cyb3r.space>
2024-06-24 08:34:27 -0400

    Save work from friday night

--- test/integration/staking_test.go
+++ test/integration/staking_test.go
@@ -46,6 +46,46 @@ func StakeAliceAsReputerTopic1(m testCommon.TestConfig) {
 	require.Equal(m.T, fmt.Sprint(stakeToAdd), aliceStakedAfter.Amount.Sub(aliceStakedBefore.Amount).String())
 }
 
+// integration tests the ability of bob to stake on alice as a reputer
+func StakeBobOnAliceAsReputerTopic1(m testCommon.TestConfig) {
+	// Record Bob stake before adding more
+	bobStakedBefore, err := m.Client.QueryEmissions().GetStakeFromDelegatorInTopicInReputer(
+		m.Ctx,
+		&emissionstypes.QueryStakeFromDelegatorInTopicInReputerRequest{
+			TopicId:          1,
+			DelegatorAddress: m.BobAddr,
+			ReputerAddress:   m.AliceAddr,
+		},
+	)
+	require.NoError(m.T, err)
+
+	const stakeToAdd = 200000
+
+	// Have bob stake
+	addDelegateStake := &emissionstypes.MsgDelegateStake{
+		Sender:  m.BobAddr,
+		Reputer: m.AliceAddr,
+		TopicId: 1,
+		Amount:  cosmosMath.NewInt(stakeToAdd),
+	}
+	txResp, err := m.Client.BroadcastTx(m.Ctx, m.BobAcc, addDelegateStake)
+	require.NoError(m.T, err)
+	_, err = m.Client.WaitForTx(m.Ctx, txResp.TxHash)
+	require.NoError(m.T, err)
+
+	// Check Alice has stake on the topic
+	bobStakedAfter, err := m.Client.QueryEmissions().GetStakeFromDelegatorInTopicInReputer(
+		m.Ctx,
+		&emissionstypes.QueryStakeFromDelegatorInTopicInReputerRequest{
+			TopicId:          1,
+			DelegatorAddress: m.BobAddr,
+			ReputerAddress:   m.AliceAddr,
+		},
+	)
+	require.NoError(m.T, err)
+	require.Equal(m.T, fmt.Sprint(stakeToAdd), bobStakedAfter.Amount.Sub(bobStakedBefore.Amount).String())
+}
+
 // Register two actors and check their registrations went through
 func StakingChecks(m testCommon.TestConfig) {
 	m.T.Log("--- Staking Alice as Reputer ---")
@@ -60,6 +100,9 @@ func StakingChecks(m testCommon.TestConfig) {
 		m.T.Log("--- Check reactivating Topic 1 ---")
 		CheckTopic1Activated(m)
 	}
+
+	m.T.Log("--- Staking Bob on Alice as Reputer ---")
+	StakeBobOnAliceAsReputerTopic1(m)
 }
 
 // Unstake Alice as a reputer in topic 1, then check success
@@ -120,8 +163,72 @@ func UnstakeAliceAsReputerTopic1(m testCommon.TestConfig) {
 	)
 }
 
+// Unstake Bob as a delegator delegated to Alice in topic 1, then check success
+func UnstakeBobAsDelegatorOnAliceTopic1(m testCommon.TestConfig) {
+	bobStake, err := m.Client.QueryEmissions().GetStakeFromDelegatorInTopicInReputer(
+		m.Ctx,
+		&emissionstypes.QueryStakeFromDelegatorInTopicInReputerRequest{
+			TopicId:          1,
+			DelegatorAddress: m.BobAddr,
+			ReputerAddress:   m.AliceAddr,
+		},
+	)
+	require.NoError(m.T, err)
+	require.True(
+		m.T,
+		bobStake.Amount.GT(cosmosMath.ZeroInt()),
+		"Bob should have stake on Alice in topic 1",
+	)
+
+	// Have Alice unstake
+	unstake := &emissionstypes.MsgRemoveDelegateStake{
+		Sender:  m.BobAddr,
+		Reputer: m.AliceAddr,
+		TopicId: 1,
+		Amount:  bobStake.Amount,
+	}
+
+	txResp, err := m.Client.BroadcastTx(m.Ctx, m.BobAcc, unstake)
+	require.NoError(m.T, err)
+	_, err = m.Client.WaitForTx(m.Ctx, txResp.TxHash)
+	require.NoError(m.T, err)
+
+	// check the unstake removal is queued
+	stakeRemoval, err := m.Client.QueryEmissions().GetDelegateStakeRemovalInfo(
+		m.Ctx,
+		&emissionstypes.QueryDelegateStakeRemovalInfoRequest{
+			TopicId:   1,
+			Delegator: m.BobAddr,
+			Reputer:   m.AliceAddr,
+		},
+	)
+	require.NoError(m.T, err)
+	require.NotNil(m.T, stakeRemoval)
+	require.NotZero(m.T, stakeRemoval.Removal.BlockRemovalCompleted)
+	m.T.Log("--- Unstake removal is queued, waiting for block ", stakeRemoval.Removal.BlockRemovalCompleted, " ---")
+	m.Client.WaitForBlockHeight(m.Ctx, stakeRemoval.Removal.BlockRemovalCompleted+1)
+
+	// Check Alice has zero stake left
+	bobStakedAfter, err := m.Client.QueryEmissions().GetStakeFromDelegatorInTopicInReputer(
+		m.Ctx,
+		&emissionstypes.QueryStakeFromDelegatorInTopicInReputerRequest{
+			TopicId:          1,
+			DelegatorAddress: m.BobAddr,
+			ReputerAddress:   m.AliceAddr,
+		},
+	)
+	require.NoError(m.T, err)
+	require.True(
+		m.T,
+		bobStakedAfter.Amount.Equal(cosmosMath.ZeroInt()),
+		"Alice should have zero stake in topic 1 after unstake",
+	)
+}
+
 // run checks for unstaking
 func UnstakingChecks(m testCommon.TestConfig) {
+	m.T.Log("--- Bob Unstaking as Delegator Upon Alice ---")
+	UnstakeBobAsDelegatorOnAliceTopic1(m)
 	m.T.Log("--- Unstaking Alice as Reputer ---")
 	UnstakeAliceAsReputerTopic1(m)
 }
