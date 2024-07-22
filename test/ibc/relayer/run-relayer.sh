4570d9f4caa3645ae6b5874e9c53b90caf5019f5
michael <michael.c@upshot.xyz>
2024-07-22 15:12:46 -0500

    add env file

--- test/ibc/relayer/run-relayer.sh
+++ test/ibc/relayer/run-relayer.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 rm -r ~/.relayer || true
 
+set -a
+source .env
+set +a
+
+echo "$ALLORA_RELAYER_MNEMONIC"
+
 rly config init
 rly chains add-dir ./configs
-rly chains add-dir ./configs1
-rly chains add-dir ./configs2
 rly paths add-dir ./paths
 
-rly keys restore allora relayer "black frequent sponsor nice claim rally hunt suit parent size stumble expire forest avocado mistake agree trend witness lounge shiver image smoke stool chicken"
-rly keys restore osmosis relayer "black frequent sponsor nice claim rally hunt suit parent size stumble expire forest avocado mistake agree trend witness lounge shiver image smoke stool chicken"
+rly keys restore allora relayer "$ALLORA_RELAYER_MNEMONIC"
+rly keys restore osmosis relayer "$OSMOSIS_RELAYER_MNEMONIC"
 
 echo "Tx Link"
 rly tx link demo -d -t 3s
