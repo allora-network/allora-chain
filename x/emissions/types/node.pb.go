187617a24518a0fc2c4fcee13a4f261439f94074
Fernando Campos <fernandofcampos@gmail.com>
2024-08-01 22:16:56 -0300

    Add back removed fields and restore the original order

--- x/emissions/types/node.pb.go
+++ x/emissions/types/node.pb.go
@@ -23,8 +23,11 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type OffchainNode struct {
-	NodeAddress string `protobuf:"bytes,1,opt,name=node_address,json=nodeAddress,proto3" json:"node_address,omitempty"`
-	Owner       string `protobuf:"bytes,2,opt,name=owner,proto3" json:"owner,omitempty"`
+	LibP2PKey    string `protobuf:"bytes,1,opt,name=lib_p2p_key,json=libP2pKey,proto3" json:"lib_p2p_key,omitempty"`
+	MultiAddress string `protobuf:"bytes,2,opt,name=multi_address,json=multiAddress,proto3" json:"multi_address,omitempty"`
+	Owner        string `protobuf:"bytes,3,opt,name=owner,proto3" json:"owner,omitempty"`
+	NodeAddress  string `protobuf:"bytes,4,opt,name=node_address,json=nodeAddress,proto3" json:"node_address,omitempty"`
+	NodeId       string `protobuf:"bytes,5,opt,name=node_id,json=nodeId,proto3" json:"node_id,omitempty"`
 }
 
 func (m *OffchainNode) Reset()         { *m = OffchainNode{} }
@@ -60,9 +63,16 @@ func (m *OffchainNode) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_OffchainNode proto.InternalMessageInfo
 
-func (m *OffchainNode) GetNodeAddress() string {
+func (m *OffchainNode) GetLibP2PKey() string {
 	if m != nil {
-		return m.NodeAddress
+		return m.LibP2PKey
+	}
+	return ""
+}
+
+func (m *OffchainNode) GetMultiAddress() string {
+	if m != nil {
+		return m.MultiAddress
 	}
 	return ""
 }
@@ -74,6 +84,20 @@ func (m *OffchainNode) GetOwner() string {
 	return ""
 }
 
+func (m *OffchainNode) GetNodeAddress() string {
+	if m != nil {
+		return m.NodeAddress
+	}
+	return ""
+}
+
+func (m *OffchainNode) GetNodeId() string {
+	if m != nil {
+		return m.NodeId
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*OffchainNode)(nil), "emissions.v1.OffchainNode")
 }
@@ -81,19 +105,23 @@ func init() {
 func init() { proto.RegisterFile("emissions/v1/node.proto", fileDescriptor_c46ef77a30f1ab81) }
 
 var fileDescriptor_c46ef77a30f1ab81 = []byte{
-	// 187 bytes of a gzipped FileDescriptorProto
+	// 252 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4f, 0xcd, 0xcd, 0x2c,
 	0x2e, 0xce, 0xcc, 0xcf, 0x2b, 0xd6, 0x2f, 0x33, 0xd4, 0xcf, 0xcb, 0x4f, 0x49, 0xd5, 0x2b, 0x28,
-	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x81, 0x4b, 0xe8, 0x95, 0x19, 0x2a, 0xb9, 0x73, 0xf1, 0xf8, 0xa7,
-	0xa5, 0x25, 0x67, 0x24, 0x66, 0xe6, 0xf9, 0xe5, 0xa7, 0xa4, 0x0a, 0x29, 0x72, 0xf1, 0x80, 0xd4,
-	0xc6, 0x27, 0xa6, 0xa4, 0x14, 0xa5, 0x16, 0x17, 0x4b, 0x30, 0x2a, 0x30, 0x6a, 0x70, 0x06, 0x71,
-	0x83, 0xc4, 0x1c, 0x21, 0x42, 0x42, 0x22, 0x5c, 0xac, 0xf9, 0xe5, 0x79, 0xa9, 0x45, 0x12, 0x4c,
-	0x60, 0x39, 0x08, 0xc7, 0x29, 0xe8, 0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c,
-	0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e, 0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0xa2,
-	0x2c, 0xd2, 0x33, 0x4b, 0x32, 0x4a, 0x93, 0xf4, 0x92, 0xf3, 0x73, 0xf5, 0x13, 0x73, 0x72, 0xf2,
-	0x8b, 0x12, 0x75, 0xf3, 0x52, 0x4b, 0xca, 0xf3, 0x8b, 0xb2, 0x61, 0x5c, 0xb0, 0xf5, 0xfa, 0x15,
-	0xfa, 0x08, 0x27, 0x97, 0x54, 0x16, 0xa4, 0x16, 0x27, 0xb1, 0x81, 0x5d, 0x6c, 0x0c, 0x08, 0x00,
-	0x00, 0xff, 0xff, 0x2c, 0xfb, 0xa9, 0xb4, 0xcc, 0x00, 0x00, 0x00,
+	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x81, 0x4b, 0xe8, 0x95, 0x19, 0x2a, 0x2d, 0x65, 0xe4, 0xe2, 0xf1,
+	0x4f, 0x4b, 0x4b, 0xce, 0x48, 0xcc, 0xcc, 0xf3, 0xcb, 0x4f, 0x49, 0x15, 0x92, 0xe3, 0xe2, 0xce,
+	0xc9, 0x4c, 0x8a, 0x2f, 0x30, 0x2a, 0x88, 0xcf, 0x4e, 0xad, 0x94, 0x60, 0x54, 0x60, 0xd4, 0xe0,
+	0x0c, 0xe2, 0xcc, 0xc9, 0x4c, 0x0a, 0x30, 0x2a, 0xf0, 0x4e, 0xad, 0x14, 0x52, 0xe6, 0xe2, 0xcd,
+	0x2d, 0xcd, 0x29, 0xc9, 0x8c, 0x4f, 0x4c, 0x49, 0x29, 0x4a, 0x2d, 0x2e, 0x96, 0x60, 0x02, 0xab,
+	0xe0, 0x01, 0x0b, 0x3a, 0x42, 0xc4, 0x84, 0x44, 0xb8, 0x58, 0xf3, 0xcb, 0xf3, 0x52, 0x8b, 0x24,
+	0x98, 0xc1, 0x92, 0x10, 0x8e, 0x90, 0x22, 0x17, 0x0f, 0xc8, 0x1d, 0x70, 0x9d, 0x2c, 0x60, 0x49,
+	0x6e, 0x90, 0x18, 0x4c, 0xa3, 0x38, 0x17, 0x3b, 0x58, 0x49, 0x66, 0x8a, 0x04, 0x2b, 0x58, 0x96,
+	0x0d, 0xc4, 0xf5, 0x4c, 0x71, 0x0a, 0x3a, 0xf1, 0x48, 0x8e, 0xf1, 0xc2, 0x23, 0x39, 0xc6, 0x07,
+	0x8f, 0xe4, 0x18, 0x27, 0x3c, 0x96, 0x63, 0xb8, 0xf0, 0x58, 0x8e, 0xe1, 0xc6, 0x63, 0x39, 0x86,
+	0x28, 0x8b, 0xf4, 0xcc, 0x92, 0x8c, 0xd2, 0x24, 0xbd, 0xe4, 0xfc, 0x5c, 0xfd, 0xc4, 0x9c, 0x9c,
+	0xfc, 0xa2, 0x44, 0xdd, 0xbc, 0xd4, 0x92, 0xf2, 0xfc, 0xa2, 0x6c, 0x18, 0x17, 0xec, 0x39, 0xfd,
+	0x0a, 0x7d, 0x44, 0x88, 0x94, 0x54, 0x16, 0xa4, 0x16, 0x27, 0xb1, 0x81, 0x03, 0xc4, 0x18, 0x10,
+	0x00, 0x00, 0xff, 0xff, 0xd7, 0xad, 0x46, 0xcd, 0x2b, 0x01, 0x00, 0x00,
 }
 
 func (m *OffchainNode) Marshal() (dAtA []byte, err error) {
@@ -116,17 +144,38 @@ func (m *OffchainNode) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.Owner) > 0 {
-		i -= len(m.Owner)
-		copy(dAtA[i:], m.Owner)
-		i = encodeVarintNode(dAtA, i, uint64(len(m.Owner)))
+	if len(m.NodeId) > 0 {
+		i -= len(m.NodeId)
+		copy(dAtA[i:], m.NodeId)
+		i = encodeVarintNode(dAtA, i, uint64(len(m.NodeId)))
 		i--
-		dAtA[i] = 0x12
+		dAtA[i] = 0x2a
 	}
 	if len(m.NodeAddress) > 0 {
 		i -= len(m.NodeAddress)
 		copy(dAtA[i:], m.NodeAddress)
 		i = encodeVarintNode(dAtA, i, uint64(len(m.NodeAddress)))
+		i--
+		dAtA[i] = 0x22
+	}
+	if len(m.Owner) > 0 {
+		i -= len(m.Owner)
+		copy(dAtA[i:], m.Owner)
+		i = encodeVarintNode(dAtA, i, uint64(len(m.Owner)))
+		i--
+		dAtA[i] = 0x1a
+	}
+	if len(m.MultiAddress) > 0 {
+		i -= len(m.MultiAddress)
+		copy(dAtA[i:], m.MultiAddress)
+		i = encodeVarintNode(dAtA, i, uint64(len(m.MultiAddress)))
+		i--
+		dAtA[i] = 0x12
+	}
+	if len(m.LibP2PKey) > 0 {
+		i -= len(m.LibP2PKey)
+		copy(dAtA[i:], m.LibP2PKey)
+		i = encodeVarintNode(dAtA, i, uint64(len(m.LibP2PKey)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -150,11 +199,23 @@ func (m *OffchainNode) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.NodeAddress)
+	l = len(m.LibP2PKey)
+	if l > 0 {
+		n += 1 + l + sovNode(uint64(l))
+	}
+	l = len(m.MultiAddress)
 	if l > 0 {
 		n += 1 + l + sovNode(uint64(l))
 	}
 	l = len(m.Owner)
+	if l > 0 {
+		n += 1 + l + sovNode(uint64(l))
+	}
+	l = len(m.NodeAddress)
+	if l > 0 {
+		n += 1 + l + sovNode(uint64(l))
+	}
+	l = len(m.NodeId)
 	if l > 0 {
 		n += 1 + l + sovNode(uint64(l))
 	}
@@ -198,7 +259,7 @@ func (m *OffchainNode) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field NodeAddress", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field LibP2PKey", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -226,9 +287,41 @@ func (m *OffchainNode) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.NodeAddress = string(dAtA[iNdEx:postIndex])
+			m.LibP2PKey = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field MultiAddress", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowNode
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthNode
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthNode
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.MultiAddress = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 3:
 			if wireType != 2 {
 				return fmt.Errorf("proto: wrong wireType = %d for field Owner", wireType)
 			}
@@ -259,6 +352,70 @@ func (m *OffchainNode) Unmarshal(dAtA []byte) error {
 				return io.ErrUnexpectedEOF
 			}
 			m.Owner = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 4:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NodeAddress", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowNode
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthNode
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthNode
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.NodeAddress = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 5:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field NodeId", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowNode
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthNode
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthNode
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.NodeId = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
