638060dc840a77d94a343db73b92207c9b7254a7
michael <michael.c@upshot.xyz>
2024-08-04 20:09:32 -0500

    add migration scripts

--- x/emissions/migrations/v2/migrations.go
+++ x/emissions/migrations/v2/migrations.go
@@ -1,8 +1,6 @@
 package v2
 
 import (
-	"context"
-
 	"cosmossdk.io/core/store"
 	"cosmossdk.io/store/prefix"
 	storetypes "cosmossdk.io/store/types"
@@ -17,7 +15,19 @@ func MigrateStore(ctx sdk.Context, storeService store.KVStoreService, cdc codec.
 	ctx.Logger().Info("MIGRATING STORE FROM VERSION 1 TO VERSION 2")
 
 	store := runtime.KVStoreAdapter(storeService.OpenKVStore(ctx))
-	err := MigrateMsgCreateNewTopic(ctx, store, cdc)
+	err := MigrateMsgCreateNewTopic(store, cdc)
+	if err != nil {
+		return err
+	}
+	err = MigrateOffchainNode(store, cdc)
+	if err != nil {
+		return err
+	}
+	err = MigrateNetworkLossBundles(store, cdc)
+	if err != nil {
+		return err
+	}
+	err = MigrateAllLossBundles(store, cdc)
 	if err != nil {
 		return err
 	}
@@ -25,7 +35,7 @@ func MigrateStore(ctx sdk.Context, storeService store.KVStoreService, cdc codec.
 	return nil
 }
 
-func MigrateMsgCreateNewTopic(ctx context.Context, store storetypes.KVStore, cdc codec.BinaryCodec) error {
+func MigrateMsgCreateNewTopic(store storetypes.KVStore, cdc codec.BinaryCodec) error {
 	topicStore := prefix.NewStore(store, emissionsv1.TopicsKey)
 	iterator := topicStore.Iterator(nil, nil)
 
@@ -53,5 +63,122 @@ func MigrateMsgCreateNewTopic(ctx context.Context, store storetypes.KVStore, cdc
 		store.Set(iterator.Key(), cdc.MustMarshal(&newMsg))
 	}
 
+	return nil
+}
+
+func MigrateOffchainNode(store storetypes.KVStore, cdc codec.BinaryCodec) error {
+	workerStore := prefix.NewStore(store, emissionsv1.WorkerNodesKey)
+	iterator := workerStore.Iterator(nil, nil)
+
+	for ; iterator.Valid(); iterator.Next() {
+		var oldMsg emissionsv1.OffchainNode
+		err := proto.Unmarshal(iterator.Value(), &oldMsg)
+		if err != nil {
+			return err
+		}
+
+		newMsg := emissionsv1.OffchainNode{
+			NodeAddress: oldMsg.NodeAddress,
+			Owner:       oldMsg.Owner,
+		}
+
+		store.Delete(iterator.Key())
+		store.Set(iterator.Key(), cdc.MustMarshal(&newMsg))
+	}
+
+	reputerStore := prefix.NewStore(store, emissionsv1.ReputerNodesKey)
+	iterator = reputerStore.Iterator(nil, nil)
+
+	for ; iterator.Valid(); iterator.Next() {
+		var oldMsg emissionsv1.OffchainNode
+		err := proto.Unmarshal(iterator.Value(), &oldMsg)
+		if err != nil {
+			return err
+		}
+
+		newMsg := emissionsv1.OffchainNode{
+			NodeAddress: oldMsg.NodeAddress,
+			Owner:       oldMsg.Owner,
+		}
+
+		store.Delete(iterator.Key())
+		store.Set(iterator.Key(), cdc.MustMarshal(&newMsg))
+	}
+	return nil
+}
+
+func MigrateNetworkLossBundles(store storetypes.KVStore, cdc codec.BinaryCodec) error {
+	networkLossBundlesStore := prefix.NewStore(store, emissionsv1.NetworkLossBundlesKey)
+	iterator := networkLossBundlesStore.Iterator(nil, nil)
+
+	for ; iterator.Valid(); iterator.Next() {
+		var oldMsg emissionsv1.ValueBundle
+		err := proto.Unmarshal(iterator.Value(), &oldMsg)
+		if err != nil {
+			return err
+		}
+
+		newMsg := emissionsv1.ValueBundle{
+			TopicId:                       oldMsg.TopicId,
+			ReputerRequestNonce:           oldMsg.ReputerRequestNonce,
+			Reputer:                       oldMsg.Reputer,
+			ExtraData:                     oldMsg.ExtraData,
+			CombinedValue:                 oldMsg.CombinedValue,
+			InfererValues:                 oldMsg.InfererValues,
+			ForecasterValues:              oldMsg.ForecasterValues,
+			NaiveValue:                    oldMsg.NaiveValue,
+			OneOutInfererForecasterValues: []*emissionsv1.OneOutInfererForecasterValues{},
+			OneOutInfererValues:           oldMsg.OneOutInfererValues,
+			OneOutForecasterValues:        oldMsg.OneOutForecasterValues,
+			OneInForecasterValues:         oldMsg.OneInForecasterValues,
+		}
+
+		store.Delete(iterator.Key())
+		store.Set(iterator.Key(), cdc.MustMarshal(&newMsg))
+	}
+	return nil
+}
+
+func MigrateAllLossBundles(store storetypes.KVStore, cdc codec.BinaryCodec) error {
+	allLossBundlesStore := prefix.NewStore(store, emissionsv1.AllLossBundlesKey)
+	iterator := allLossBundlesStore.Iterator(nil, nil)
+
+	for ; iterator.Valid(); iterator.Next() {
+		var oldMsg emissionsv1.ReputerValueBundles
+		err := proto.Unmarshal(iterator.Value(), &oldMsg)
+		if err != nil {
+			return err
+		}
+
+		newMsg := emissionsv1.ReputerValueBundles{
+			ReputerValueBundles: []*emissionsv1.ReputerValueBundle{},
+		}
+
+		for _, valueBundle := range oldMsg.ReputerValueBundles {
+			newMsg.ReputerValueBundles = append(newMsg.ReputerValueBundles,
+				&emissionsv1.ReputerValueBundle{
+					ValueBundle: &emissionsv1.ValueBundle{
+						TopicId:                       valueBundle.ValueBundle.TopicId,
+						ReputerRequestNonce:           valueBundle.ValueBundle.ReputerRequestNonce,
+						Reputer:                       valueBundle.ValueBundle.Reputer,
+						ExtraData:                     valueBundle.ValueBundle.ExtraData,
+						CombinedValue:                 valueBundle.ValueBundle.CombinedValue,
+						InfererValues:                 valueBundle.ValueBundle.InfererValues,
+						ForecasterValues:              valueBundle.ValueBundle.ForecasterValues,
+						NaiveValue:                    valueBundle.ValueBundle.NaiveValue,
+						OneOutInfererForecasterValues: []*emissionsv1.OneOutInfererForecasterValues{},
+						OneOutInfererValues:           valueBundle.ValueBundle.OneOutInfererValues,
+						OneOutForecasterValues:        valueBundle.ValueBundle.OneOutForecasterValues,
+						OneInForecasterValues:         valueBundle.ValueBundle.OneInForecasterValues,
+					},
+					Pubkey:    valueBundle.Pubkey,
+					Signature: valueBundle.Signature,
+				},
+			)
+		}
+
+		store.Delete(iterator.Key())
+		store.Set(iterator.Key(), cdc.MustMarshal(&newMsg))
+	}
 	return nil
 }
