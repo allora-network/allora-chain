b194d7cc6180fdc437e838529e756541d5ac0ee3
Tyler <tyler@cyb3r.space>
2024-08-23 11:49:54 -0400

    file renames

--- x/emissions/keeper/actor_utils/sortition.go
+++ x/emissions/keeper/actor_utils/sortition.go
@@ -1,12 +1,61 @@
 package actorutils
 
 import (
-	alloraMath "github.com/allora-network/allora-chain/math"
-	emissionskeeper "github.com/allora-network/allora-chain/x/emissions/keeper"
+	"math/rand"
+	"slices"
+
 	emissionstypes "github.com/allora-network/allora-chain/x/emissions/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// Sorts the given actors by score, desc, breaking ties randomly
+// Returns the top N actors as a map with the actor as the key and a boolean (True) as the value
+func FindTopNByScoreDesc(
+	ctx sdk.Context,
+	n uint64,
+	scores []emissionstypes.Score,
+	randSeed int64,
+) (topNActorsSorted []emissionstypes.Score, allActorsSorted []emissionstypes.Score, actorIsTop map[string]struct{}) {
+	r := rand.New(rand.NewSource(randSeed)) //nolint:gosec // G404: Use of weak random number generator (math/rand or math/rand/v2 instead of crypto/rand)
+	// in our tiebreaker, we never return that two elements are equal
+	// so the sort function will never be called with two equal elements
+	// so we don't care about sort stability because our tiebreaker determines the order
+	// we also sort from largest to smallest so the sort function is inverted
+	// from the usual smallest to largest sort order
+	slices.SortFunc(scores, func(x, y emissionstypes.Score) int {
+		if x.Score.Lt(y.Score) {
+			return 1
+		} else if x.Score.Gt(y.Score) {
+			return -1
+		} else {
+			tiebreaker := r.Intn(2)
+			if tiebreaker == 0 {
+				return -1
+			} else {
+				return 1
+			}
+		}
+	})
+
+	// which is bigger, n or the length of the scores?
+	N := n
+	if N > uint64(len(scores)) {
+		N = uint64(len(scores))
+	}
+	topNActorsSorted = make([]emissionstypes.Score, N)
+	actorIsTop = make(map[string]struct{}, N)
+	// populate top n actors sorted with only the top n
+	// populate all with all
+	// actor is top is a map of the top n actors
+	for i := uint64(0); i < N; i++ {
+		topNActorsSorted[i] = scores[i]
+		actorIsTop[scores[i].Address] = struct{}{}
+	}
+
+	return topNActorsSorted, scores, actorIsTop
+}
+
+/*
 // Returns the percentile-ith value of the given sorted scores
 // e.g. if percentile is 25%, for all the scores sorted from greatest to smallest
 // give me the value that is greater than 25% of the values and less than 75% of the values
@@ -127,3 +176,5 @@ func UpdateScoresOfPassiveActorsWithActivePercentile(
 	}
 	return nil
 }
+
+*/
--- x/emissions/keeper/actor_utils/sortition_test.go
+++ x/emissions/keeper/actor_utils/sortition_test.go
@@ -13,22 +13,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindTopNByScoreDesc(t *testing.T) {
-	key := storetypes.NewKVStoreKey("test")
-	topicId := uint64(0)
-	testCtx := testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test")).Ctx
-
+// test helper function reduce code duplication
+func setUpSomeWorkers(t *testing.T) (
+	testCtx sdk.Context,
+	worker1Addr sdk.AccAddress,
+	worker2Addr sdk.AccAddress,
+	worker3Addr sdk.AccAddress,
+	worker4Addr sdk.AccAddress,
+	worker5Addr sdk.AccAddress,
+) {
 	worker1PrivateKey := secp256k1.GenPrivKey()
 	worker2PrivateKey := secp256k1.GenPrivKey()
 	worker3PrivateKey := secp256k1.GenPrivKey()
 	worker4PrivateKey := secp256k1.GenPrivKey()
 	worker5PrivateKey := secp256k1.GenPrivKey()
 
-	worker1Addr := sdk.AccAddress(worker1PrivateKey.PubKey().Address())
-	worker2Addr := sdk.AccAddress(worker2PrivateKey.PubKey().Address())
-	worker3Addr := sdk.AccAddress(worker3PrivateKey.PubKey().Address())
-	worker4Addr := sdk.AccAddress(worker4PrivateKey.PubKey().Address())
-	worker5Addr := sdk.AccAddress(worker5PrivateKey.PubKey().Address())
+	worker1Addr = sdk.AccAddress(worker1PrivateKey.PubKey().Address())
+	worker2Addr = sdk.AccAddress(worker2PrivateKey.PubKey().Address())
+	worker3Addr = sdk.AccAddress(worker3PrivateKey.PubKey().Address())
+	worker4Addr = sdk.AccAddress(worker4PrivateKey.PubKey().Address())
+	worker5Addr = sdk.AccAddress(worker5PrivateKey.PubKey().Address())
+
+	key := storetypes.NewKVStoreKey("test")
+	testCtx = testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test")).Ctx
+
+	return testCtx, worker1Addr, worker2Addr, worker3Addr, worker4Addr, worker5Addr
+}
+
+func TestFindTopNByScoreDesc(t *testing.T) {
+	topicId := uint64(0)
+	testCtx,
+		worker1Addr,
+		worker2Addr,
+		worker3Addr,
+		worker4Addr,
+		worker5Addr := setUpSomeWorkers(t)
 
 	latestReputerScores := []types.Score{
 		{TopicId: topicId, BlockHeight: 1, Address: worker1Addr.String(), Score: alloraMath.NewDecFromInt64(90)},
@@ -57,28 +76,26 @@ func TestFindTopNByScoreDesc(t *testing.T) {
 
 func TestFindTopNByScoreDescWithNils(t *testing.T) {
 	topicId := uint64(0)
+	testCtx, _, _, worker3Addr, _, worker5Addr := setUpSomeWorkers(t)
 
-	worker1PrivateKey := secp256k1.GenPrivKey()
-	worker3PrivateKey := secp256k1.GenPrivKey()
-	worker5PrivateKey := secp256k1.GenPrivKey()
-
-	worker1Addr := sdk.AccAddress(worker1PrivateKey.PubKey().Address())
-	worker3Addr := sdk.AccAddress(worker3PrivateKey.PubKey().Address())
-	worker5Addr := sdk.AccAddress(worker5PrivateKey.PubKey().Address())
-
-	ReputerScoreEmas := make(map[string]types.Score)
-	ReputerScoreEmas[worker1Addr.String()] = types.Score{}
-	ReputerScoreEmas[worker3Addr.String()] = types.Score{TopicId: topicId, BlockHeight: 1, Address: worker3Addr.String(), Score: alloraMath.NewDecFromInt64(80)}
-	ReputerScoreEmas[worker5Addr.String()] = types.Score{TopicId: topicId, BlockHeight: 1, Address: worker5Addr.String(), Score: alloraMath.NewDecFromInt64(100)}
+	reputerScoreEmas := []types.Score{
+		{TopicId: topicId, BlockHeight: 1, Address: worker3Addr.String(), Score: alloraMath.NewDecFromInt64(80)},
+		{TopicId: topicId, BlockHeight: 1, Address: worker5Addr.String(), Score: alloraMath.NewDecFromInt64(100)},
+		{}, //nolint:exhaustruct
+	}
 
 	// Actors with nil scores sent to the end
-	topActors, allActorsSorted := actorutils.FindTopNByScoreDesc(3, ReputerScoreEmas, 1)
+	topActors, allActorsSorted, actorIsTop := actorutils.FindTopNByScoreDesc(testCtx, 3, reputerScoreEmas, 1)
 	require.Equal(t, 3, len(topActors))
-	require.Equal(t, worker5Addr.String(), topActors[0])
-	require.Equal(t, worker3Addr.String(), topActors[1])
-	require.Equal(t, worker1Addr.String(), topActors[2])
+	require.Equal(t, worker5Addr.String(), topActors[0].Address)
+	require.Equal(t, struct{}{}, actorIsTop[worker5Addr.String()])
+	require.Equal(t, worker3Addr.String(), topActors[1].Address)
+	require.Equal(t, struct{}{}, actorIsTop[worker3Addr.String()])
+	require.Equal(t, "", topActors[2].Address)
 	require.Equal(t, 3, len(allActorsSorted))
 }
+
+/*
 
 func TestGetQuantileOfDescSliceAsAsc(t *testing.T) {
 	scoresByActor := map[Actor]Score{
@@ -109,3 +126,4 @@ func TestGetQuantileOfDescSliceAsAsc(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, expectedInt, actualInt)
 }
+*/
