d244f6c75fb3d8bef2dc73afe4826933e81343e7
Tyler <tyler@cyb3r.space>
2024-08-23 12:56:03 -0400

    isTop test

--- x/emissions/keeper/actor_utils/sortition.go
+++ x/emissions/keeper/actor_utils/sortition.go
@@ -54,3 +54,127 @@ func FindTopNByScoreDesc(
 
 	return topNActorsSorted, scores, actorIsTop
 }
+
+/*
+// Returns the percentile-ith value of the given sorted scores
+// e.g. if percentile is 25%, for all the scores sorted from greatest to smallest
+// give me the value that is greater than 25% of the values and less than 75% of the values
+func GetPercentileOfScores(
+	sortedScores []emissionstypes.Score,
+	percentile alloraMath.Dec,
+) (alloraMath.Dec, error) {
+	if len(sortedIdsOfValues) == 0 {
+		return alloraMath.Dec{}, emissionstypes.ErrEmptyArray
+	}
+
+	// position = (1 - q) * (n - 1)
+	nLessOne, err := alloraMath.NewDecFromUint64(uint64(len(sortedIdsOfValues) - 1))
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	oneLessQ, err := alloraMath.OneDec().Sub(quantile)
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	position, err := oneLessQ.Mul(nLessOne)
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+
+	lowerIndex, err := position.Floor()
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	lowerIndexInt, err := lowerIndex.Int64()
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	upperIndex, err := position.Ceil()
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	upperIndexInt, err := upperIndex.Int64()
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+
+	if lowerIndex == upperIndex {
+		return scoresByActor[sortedIdsOfValues[lowerIndexInt]].Score, nil
+	}
+
+	// return lowerValue + (upperValue-lowerValue)*(position-float64(lowerIndex))
+	lowerScore := scoresByActor[sortedIdsOfValues[lowerIndexInt]].Score
+	upperScore := scoresByActor[sortedIdsOfValues[upperIndexInt]].Score
+	positionMinusLowerIndex, err := position.Sub(lowerIndex)
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	upperMinusLower, err := upperScore.Sub(lowerScore)
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	product, err := positionMinusLowerIndex.Mul(upperMinusLower)
+	if err != nil {
+		return alloraMath.Dec{}, err
+	}
+	return lowerScore.Add(product)
+}
+
+func UpdateScoresOfPassiveActorsWithActivePercentile(
+	ctx sdk.Context,
+	k *emissionskeeper.Keeper,
+	blockHeight int64,
+	topicId uint64,
+	alphaRegret alloraMath.Dec,
+	topicPercentile alloraMath.Dec,
+	topScoresSorted []emissionstypes.Score,
+	allScoresSorted []emissionstypes.Score,
+	actorIsTop map[string]struct{},
+	actorType emissionstypes.ActorType,
+) error {
+	// if the length of topScoresSorted is the
+	// same as allScoresSorted, then all actors are top actors
+	// and we don't have to do anything
+	if len(topScoresSorted) == len(allScoresSorted) {
+		return nil
+	}
+
+	percentile, err := GetPercentileOfScores(topScoresSorted, topicPercentile)
+	if err != nil {
+		return err
+	}
+	// Update score EMAs of all actors not in topActors
+	for _, actor := range allActorsSorted {
+		if _, ok := actorIsTop[actor.Address]; !ok {
+			// Update the score EMA
+			newScore := emissionstypes.Score{
+				TopicId:     topicId,
+				BlockHeight: blockHeight,
+				Address:     actor.Address,
+				Score:       percentile,
+			}
+			switch actorType {
+			case emissionstypes.ActorType_INFERER:
+				err := k.UpdateInfererScoreEma(ctx, topicId, alphaRegret, actor.Address, newScore)
+				if err != nil {
+					return err
+				}
+			case emissionstypes.ActorType_FORECASTER:
+				err := k.UpdateForecasterScoreEma(ctx, topicId, alphaRegret, actor.Address, newScore)
+				if err != nil {
+					return err
+				}
+			case emissionstypes.ActorType_REPUTER:
+				err := k.UpdateReputerScoreEma(ctx, topicId, alphaRegret, actor.Address, newScore)
+				if err != nil {
+					return err
+				}
+			default:
+				return emissionstypes.ErrInvalidActorType
+			}
+		}
+	}
+	return nil
+}
+
+*/
--- x/emissions/keeper/actor_utils/sortition_test.go
+++ x/emissions/keeper/actor_utils/sortition_test.go
@@ -1,10 +1,14 @@
-package actorutils
+package actorutils_test
 
 import (
+	"fmt"
+	"strconv"
 	"testing"
 
 	storetypes "cosmossdk.io/store/types"
 	alloraMath "github.com/allora-network/allora-chain/math"
+	alloratestutil "github.com/allora-network/allora-chain/test/testutil"
+	actorutils "github.com/allora-network/allora-chain/x/emissions/keeper/actor_utils"
 	"github.com/allora-network/allora-chain/x/emissions/types"
 	"github.com/cometbft/cometbft/crypto/secp256k1"
 	"github.com/cosmos/cosmos-sdk/testutil"
@@ -12,24 +16,44 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestFindTopNByScoreDesc(t *testing.T) {
-	key := storetypes.NewKVStoreKey("test")
-	topicId := uint64(0)
-	testCtx := testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test")).Ctx
-
+// test helper function reduce code duplication
+func setUpSomeWorkers(t *testing.T) (
+	testCtx sdk.Context,
+	worker1Addr sdk.AccAddress,
+	worker2Addr sdk.AccAddress,
+	worker3Addr sdk.AccAddress,
+	worker4Addr sdk.AccAddress,
+	worker5Addr sdk.AccAddress,
+) {
 	worker1PrivateKey := secp256k1.GenPrivKey()
 	worker2PrivateKey := secp256k1.GenPrivKey()
 	worker3PrivateKey := secp256k1.GenPrivKey()
 	worker4PrivateKey := secp256k1.GenPrivKey()
 	worker5PrivateKey := secp256k1.GenPrivKey()
 
-	worker1Addr := sdk.AccAddress(worker1PrivateKey.PubKey().Address())
-	worker2Addr := sdk.AccAddress(worker2PrivateKey.PubKey().Address())
-	worker3Addr := sdk.AccAddress(worker3PrivateKey.PubKey().Address())
-	worker4Addr := sdk.AccAddress(worker4PrivateKey.PubKey().Address())
-	worker5Addr := sdk.AccAddress(worker5PrivateKey.PubKey().Address())
+	worker1Addr = sdk.AccAddress(worker1PrivateKey.PubKey().Address())
+	worker2Addr = sdk.AccAddress(worker2PrivateKey.PubKey().Address())
+	worker3Addr = sdk.AccAddress(worker3PrivateKey.PubKey().Address())
+	worker4Addr = sdk.AccAddress(worker4PrivateKey.PubKey().Address())
+	worker5Addr = sdk.AccAddress(worker5PrivateKey.PubKey().Address())
 
-	latestReputerScores := []types.Score{
+	key := storetypes.NewKVStoreKey("test")
+	testCtx = testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test")).Ctx
+
+	return testCtx, worker1Addr, worker2Addr, worker3Addr, worker4Addr, worker5Addr
+}
+
+// basic sanity test
+func TestFindTopNByScoreDesc(t *testing.T) {
+	topicId := uint64(0)
+	testCtx,
+		worker1Addr,
+		worker2Addr,
+		worker3Addr,
+		worker4Addr,
+		worker5Addr := setUpSomeWorkers(t)
+
+	reputerScoreEmas := []types.Score{
 		{TopicId: topicId, BlockHeight: 1, Address: worker1Addr.String(), Score: alloraMath.NewDecFromInt64(90)},
 		{TopicId: topicId, BlockHeight: 1, Address: worker2Addr.String(), Score: alloraMath.NewDecFromInt64(40)},
 		{TopicId: topicId, BlockHeight: 1, Address: worker3Addr.String(), Score: alloraMath.NewDecFromInt64(80)},
@@ -37,7 +61,7 @@ func TestFindTopNByScoreDesc(t *testing.T) {
 		{TopicId: topicId, BlockHeight: 1, Address: worker5Addr.String(), Score: alloraMath.NewDecFromInt64(100)},
 	}
 
-	topActors, _, topActorsBool := FindTopNByScoreDesc(testCtx, 3, latestReputerScores, 1)
+	topActors, _, topActorsBool := actorutils.FindTopNByScoreDesc(testCtx, 3, reputerScoreEmas, 1)
 	require.Equal(t, worker5Addr.String(), topActors[0].Address)
 	require.Equal(t, worker1Addr.String(), topActors[1].Address)
 	require.Equal(t, worker3Addr.String(), topActors[2].Address)
@@ -53,3 +77,109 @@ func TestFindTopNByScoreDesc(t *testing.T) {
 	_, isTop = topActorsBool[worker5Addr.String()]
 	require.Equal(t, isTop, true)
 }
+
+// helper function that handles the expected types of the top actors
+func requireIsTop(t *testing.T, expectedTop alloraMath.Dec, isTop bool) {
+	t.Helper()
+	require.True(
+		t, expectedTop.Equal(alloraMath.OneDec()) || expectedTop.Equal(alloraMath.ZeroDec()),
+		"expectedTop must be 0 or 1, got %s", expectedTop.String(),
+	)
+	expectedTopBool := expectedTop.Equal(alloraMath.OneDec())
+	require.Equal(t, expectedTopBool, isTop)
+}
+
+func TestFindTopNByScoreDescCsv(t *testing.T) {
+	for epoch := 301; epoch < 400; epoch++ {
+		epochGet := alloratestutil.GetSortitionSimulatorValuesGetterForEpochs()[epoch]
+		key := storetypes.NewKVStoreKey("test")
+		testCtx := testutil.DefaultContextWithDB(t, key, storetypes.NewTransientStoreKey("transient_test")).Ctx
+		topicId := uint64(0)
+
+		nParticipants, err := epochGet("n_participants").UInt64()
+		reputerScoreEmas := make([]types.Score, 0)
+		for i := 0; uint64(i) < nParticipants; i++ {
+			participantName := strconv.Itoa(i)
+			reputerScoreEmas = append(reputerScoreEmas, types.Score{
+				TopicId:     topicId,
+				BlockHeight: int64(epoch),
+				Address:     participantName,
+				Score:       epochGet(fmt.Sprintf("%s_prev_quality_ema", participantName)),
+			})
+		}
+
+		nParticipantsDrawn, err := epochGet("n_participants_drawn").UInt64()
+		require.NoError(t, err)
+
+		_, _, topActorsBool := actorutils.FindTopNByScoreDesc(
+			testCtx,
+			nParticipantsDrawn,
+			reputerScoreEmas,
+			1,
+		)
+
+		for i := 0; uint64(i) < nParticipants; i++ {
+			participantName := strconv.Itoa(i)
+			expectedTop := epochGet(fmt.Sprintf("%s_active", participantName))
+			_, isTop := topActorsBool[participantName]
+			requireIsTop(t, expectedTop, isTop)
+		}
+	}
+}
+
+// Not convinced we should be not throwing errors in FindTopNbyScoreDesc
+// but for now instead of throwing errors, we find top N including empty scores
+// and just file empty scores at the end of the list
+func TestFindTopNByScoreDescWithNils(t *testing.T) {
+	topicId := uint64(0)
+	testCtx, _, _, worker3Addr, _, worker5Addr := setUpSomeWorkers(t)
+
+	reputerScoreEmas := []types.Score{
+		{TopicId: topicId, BlockHeight: 1, Address: worker3Addr.String(), Score: alloraMath.NewDecFromInt64(80)},
+		{TopicId: topicId, BlockHeight: 1, Address: worker5Addr.String(), Score: alloraMath.NewDecFromInt64(100)},
+		{}, //nolint:exhaustruct
+	}
+
+	// Actors with nil scores sent to the end
+	topActors, allActorsSorted, actorIsTop := actorutils.FindTopNByScoreDesc(testCtx, 3, reputerScoreEmas, 1)
+	require.Equal(t, 3, len(topActors))
+	require.Equal(t, worker5Addr.String(), topActors[0].Address)
+	require.Equal(t, struct{}{}, actorIsTop[worker5Addr.String()])
+	require.Equal(t, worker3Addr.String(), topActors[1].Address)
+	require.Equal(t, struct{}{}, actorIsTop[worker3Addr.String()])
+	require.Equal(t, "", topActors[2].Address)
+	require.Equal(t, 3, len(allActorsSorted))
+}
+
+/*
+
+func TestGetQuantileOfDescSliceAsAsc(t *testing.T) {
+	scoresByActor := map[Actor]Score{
+		"w1": types.Score{Score: alloraMath.NewDecFromInt64(90)},
+		"w2": types.Score{Score: alloraMath.NewDecFromInt64(80)},
+		"w3": types.Score{Score: alloraMath.NewDecFromInt64(70)},
+		"w4": types.Score{Score: alloraMath.NewDecFromInt64(60)},
+		"w5": types.Score{Score: alloraMath.NewDecFromInt64(50)},
+	}
+
+	sortedSlice := []Actor{"w1", "w2", "w3", "w4", "w5"}
+
+	quantile := alloraMath.MustNewDecFromString("0.5")
+	expectedResult := alloraMath.NewDecFromInt64(70)
+
+	result, err := actorutils.GetQuantileOfDescSliceAsAsc(scoresByActor, sortedSlice, quantile)
+	require.NoError(t, err)
+	require.Equal(t, expectedResult, result)
+
+	quantile = alloraMath.MustNewDecFromString("0.2")
+	expectedResult = alloraMath.NewDecFromInt64(58)
+
+	result, err = actorutils.GetQuantileOfDescSliceAsAsc(scoresByActor, sortedSlice, quantile)
+	require.NoError(t, err)
+	expectedInt, err := expectedResult.Int64()
+	require.NoError(t, err)
+	actualInt, err := result.Int64()
+	require.NoError(t, err)
+	require.Equal(t, expectedInt, actualInt)
+}
+*/
