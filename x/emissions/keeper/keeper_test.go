8480cf4e38c9006d9989efabc8e63073263fbb2a
Tyler <tyler@cyb3r.space>
2024-08-29 21:45:39 -0400

    in tests, add topic fields when creating

--- x/emissions/keeper/keeper_test.go
+++ x/emissions/keeper/keeper_test.go
@@ -184,15 +184,19 @@ func (s *KeeperTestSuite) CreateOneTopic(epochLen int64) uint64 {
 	creator := sdk.AccAddress(PKS[0].Address())
 
 	newTopicMsg := &types.MsgCreateNewTopic{
-		Creator:                creator.String(),
-		Metadata:               metadata,
-		LossMethod:             "method",
-		EpochLength:            epochLen,
-		GroundTruthLag:         epochLen,
-		WorkerSubmissionWindow: epochLen,
-		AlphaRegret:            alloraMath.NewDecFromInt64(1),
-		PNorm:                  alloraMath.NewDecFromInt64(3),
-		Epsilon:                alloraMath.MustNewDecFromString("0.01"),
+		Creator:                  creator.String(),
+		Metadata:                 metadata,
+		LossMethod:               "method",
+		EpochLength:              epochLen,
+		GroundTruthLag:           epochLen,
+		WorkerSubmissionWindow:   epochLen,
+		AlphaRegret:              alloraMath.NewDecFromInt64(1),
+		PNorm:                    alloraMath.NewDecFromInt64(3),
+		Epsilon:                  alloraMath.MustNewDecFromString("0.01"),
+		MeritSortitionAlpha:      alloraMath.MustNewDecFromString("0.1"),
+		ActiveInfererQuantile:    alloraMath.MustNewDecFromString("0.2"),
+		ActiveForecasterQuantile: alloraMath.MustNewDecFromString("0.2"),
+		ActiveReputerQuantile:    alloraMath.MustNewDecFromString("0.2"),
 	}
 
 	s.MintTokensToAddress(creator, types.DefaultParams().CreateTopicFee)
