57cfcb8ba44ff1938e0a4ba180777cd29ea412a1
Guilherme Brandão <guilherme1237@gmail.com>
2024-10-12 12:55:09 +0100

    Fix linter

--- x/emissions/keeper/genesis.go
+++ x/emissions/keeper/genesis.go
@@ -2019,7 +2019,7 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		})
 	}
 
-	lowestInfererScoreEmas := make([]*types.TopicIdActorIdScore, 0)
+	lowestInfererScoreEma := make([]*types.TopicIdActorIdScore, 0)
 	lowestInfererScoreEmaIter, err := k.lowestInfererScoreEma.Iterate(ctx, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to iterate lowest inferer score emas")
@@ -2029,14 +2029,14 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to get key value: lowestInfererScoreEmaIter")
 		}
-		lowestInfererScoreEmas = append(lowestInfererScoreEmas, &types.TopicIdActorIdScore{
+		lowestInfererScoreEma = append(lowestInfererScoreEma, &types.TopicIdActorIdScore{
 			TopicId: keyValue.Key,
 			ActorId: keyValue.Value.Address,
 			Score:   &keyValue.Value,
 		})
 	}
 
-	lowestForecasterScoreEmas := make([]*types.TopicIdActorIdScore, 0)
+	lowestForecasterScoreEma := make([]*types.TopicIdActorIdScore, 0)
 	lowestForecasterScoreEmaIter, err := k.lowestForecasterScoreEma.Iterate(ctx, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to iterate lowest forecaster score emas")
@@ -2046,7 +2046,7 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to get key value: lowestForecasterScoreEmaIter")
 		}
-		lowestForecasterScoreEmas = append(lowestForecasterScoreEmas, &types.TopicIdActorIdScore{
+		lowestForecasterScoreEma = append(lowestForecasterScoreEma, &types.TopicIdActorIdScore{
 			TopicId: keyValue.Key,
 			ActorId: keyValue.Value.Address,
 			Score:   &keyValue.Value,
@@ -2069,7 +2069,7 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		})
 	}
 
-	lowestReputerScoreEmas := make([]*types.TopicIdActorIdScore, 0)
+	lowestReputerScoreEma := make([]*types.TopicIdActorIdScore, 0)
 	lowestReputerScoreEmaIter, err := k.lowestReputerScoreEma.Iterate(ctx, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to iterate lowest reputer score emas")
@@ -2079,7 +2079,7 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to get key value: lowestReputerScoreEmaIter")
 		}
-		lowestReputerScoreEmas = append(lowestReputerScoreEmas, &types.TopicIdActorIdScore{
+		lowestReputerScoreEma = append(lowestReputerScoreEma, &types.TopicIdActorIdScore{
 			TopicId: keyValue.Key,
 			ActorId: keyValue.Value.Address,
 			Score:   &keyValue.Value,
@@ -2204,10 +2204,11 @@ func (k *Keeper) ExportGenesis(ctx context.Context) (*types.GenesisState, error)
 		PreviousTopicQuantileReputerScoreEma:           previousTopicQuantileReputerScoreEma,
 		ActiveInferers:                                 activeInferers,
 		ActiveForecasters:                              activeForecasters,
-		LowestInfererScoreEmas:                         lowestInfererScoreEmas,
-		LowestForecasterScoreEmas:                      lowestForecasterScoreEmas,
 		ActiveReputers:                                 activeReputers,
-		LowestReputerScoreEmas:                         lowestReputerScoreEmas,
+		LowestInfererScoreEma:                          lowestInfererScoreEma,
+		LowestForecasterScoreEma:                       lowestForecasterScoreEma,
+		LowestReputerScoreEma:                          lowestReputerScoreEma,
+		LossBundles:                                    lossBundles,
 		CountInfererInclusionsInTopicActiveSet:         countInfererInclusionsInTopicActiveSet,
 		CountForecasterInclusionsInTopicActiveSet:      countForecasterInclusionsInTopicActiveSet,
 	}, nil
