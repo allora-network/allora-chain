c78b51dca82411e46c01e87750031f0539cef194
Diego Campo <xmariachi@gmail.com>
2024-12-17 03:52:56 +0100

    autocli using diff v for q than tx

--- x/emissions/module/autocli.go
+++ x/emissions/module/autocli.go
@@ -3,13 +3,14 @@ package module
 import (
 	autocliv1 "cosmossdk.io/api/cosmos/autocli/v1"
 	statev6 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v6"
+	statev7 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v7"
 )
 
 // AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
 func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 	return &autocliv1.ModuleOptions{
 		Query: &autocliv1.ServiceCommandDescriptor{
-			Service: statev6.QueryService_ServiceDesc.ServiceName,
+			Service: statev7.QueryService_ServiceDesc.ServiceName,
 			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
 				{
 					RpcMethod: "GetParams",
