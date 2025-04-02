6d342182dc6388819e819072284c3b12b75720b2
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-04-02 02:00:33 +0200

    fix(math): implement ln in a deterministic way

--- math/dec.go
+++ math/dec.go
@@ -50,14 +50,38 @@ var (
 	ErrOutOfRange         = errorsmod.Register(mathCodespace, 8, "value is out of range")
 )
 
+var (
+	logOfEbase2 = MustNewDecFromString("1.442695040888963407359924681001892137")
+	// 0.5 with sufficient precision digit to allow right shifting keeping dec128 precision
+	log2B     apd.Decimal
+	oneBigInt = apd.NewBigInt(1)
+	zeroDec   = apd.New(0, 0)
+	oneDec    = apd.New(1, 0)
+	twoDec    = apd.New(2, 0)
+)
+
+func init() {
+	b, _, err := apd.NewFromString("0.5")
+	if err != nil {
+		panic(err)
+	}
+	b.Coeff.Mul(&b.Coeff, apd.NewBigInt(10).Exp(apd.NewBigInt(10), apd.NewBigInt(int64(dec128Context.Precision+1)), nil))
+	b.Exponent -= int32(dec128Context.Precision + 1)
+	log2B = *b
+}
+
 // The number 0 encoded as Dec
 func ZeroDec() Dec {
-	return NewDecFromInt64(0)
+	var d apd.Decimal
+	d.Set(zeroDec)
+	return Dec{dec: d, isNaN: false}
 }
 
 // The number 1 encoded as Dec
 func OneDec() Dec {
-	return NewDecFromInt64(1)
+	var d apd.Decimal
+	d.Set(oneDec)
+	return Dec{dec: d, isNaN: false}
 }
 
 // In cosmos-sdk#7773, decimal128 (with 34 digits of precision) was suggested for performing
@@ -388,18 +412,78 @@ func Log10(x Dec) (Dec, error) {
 
 // Ln returns a new Dec with the value of the natural logarithm of x, without mutating x.
 func Ln(x Dec) (Dec, error) {
-	if x.IsNaN() {
-		return Dec{}, errorsmod.Wrapf(ErrNaN, "cannot Ln a NaN %s", x.String())
-	}
-	var z Dec
-	_, err := dec128Context.Ln(&z.dec, &x.dec)
-	if z.IsNaN() {
-		return z, errorsmod.Wrapf(ErrNaN, "Ln result is NaN %s", x.String())
-	}
+	log2x, err := Log2(x)
 	if err != nil {
-		return z, errorsmod.Wrapf(err, "decimal natural logarithm error %s", x.String())
+		return Dec{}, errorsmod.Wrap(ErrNaN, "decimal natural logarithm error")
 	}
-	return z, nil
+
+	return log2x.Quo(logOfEbase2)
+}
+
+// Log2 returns a new Dec with the value of the base 2 logarithm of x, without mutating x.
+// It implements the algorithm described in http://www.claysturner.com/dsp/binarylogarithm.pdf
+// And inspired from Osmosis implementation: https://github.com/osmosis-labs/osmosis/blob/927d940cf569e25e5a21911a77d5c81a0c5a0dc6/osmomath/decimal.go#L1070
+func Log2(x Dec) (Dec, error) {
+	if x.IsNaN() {
+		return Dec{}, errorsmod.Wrapf(ErrNaN, "cannot Log2 a NaN %s", x.String())
+	}
+	if x.dec.Sign() < 0 {
+		return Dec{}, errorsmod.Wrap(ErrNaN, "cannot Log2 a negative value")
+	}
+
+	// Increase precision for result accuracy.
+	decCtx := dec128Context.WithPrecision(dec128Context.Precision + 1)
+
+	var xCopy apd.Decimal
+	xCopy.Set(&x.dec)
+
+	// As we'll make divisions by 2 through bit right shift, we need to make sure we have enough digit precision in the coeff.
+	// For example:
+	// Taking a 2.12 dec represented by {coeff: 212, exp: -2}
+	// It is here transformed to {coeff: 212000000000000000000000000000000000, exp: -35}
+	precDelta := int64(decCtx.Precision) + min(int64(xCopy.Exponent), 0)
+	if precDelta > 0 && int64(xCopy.Exponent)-precDelta >= goMath.MinInt32 {
+		ten := apd.NewBigInt(10)
+		xCopy.Coeff.Mul(&xCopy.Coeff, ten.Exp(ten, apd.NewBigInt(precDelta), nil))
+		xCopy.Exponent -= int32(precDelta)
+	}
+
+	yBig := apd.NewBigInt(0)
+
+	for xCopy.Cmp(oneDec) == -1 {
+		xCopy.Coeff.Lsh(&xCopy.Coeff, 1)
+		yBig.Sub(yBig, oneBigInt)
+	}
+
+	if xCopy.Cmp(twoDec) >= 0 {
+		for xCopy.Cmp(twoDec) >= 0 {
+			xCopy.Coeff.Rsh(&xCopy.Coeff, 1)
+			yBig.Add(yBig, oneBigInt)
+		}
+	}
+
+	var b apd.Decimal
+	b.Set(&log2B)
+	y := apd.NewWithBigInt(yBig, 0)
+
+	// The more iterations, the more accurate the result.
+	// 200 iterations seems enough for 34 precision when not too close from log limits.
+	for i := 0; i < 200; i++ {
+		_, err := decCtx.Mul(&xCopy, &xCopy, &xCopy)
+		if err != nil {
+			return Dec{}, errorsmod.Wrap(ErrNaN, "decimal binary logarithm error")
+		}
+		if xCopy.Cmp(twoDec) >= 0 {
+			xCopy.Coeff.Rsh(&xCopy.Coeff, 1)
+			_, err = apd.BaseContext.Add(y, y, &b)
+			if err != nil {
+				return Dec{}, errorsmod.Wrap(ErrNaN, "decimal binary logarithm error")
+			}
+		}
+		b.Coeff.Rsh(&b.Coeff, 1)
+	}
+
+	return Dec{dec: *y}, nil
 }
 
 // Exp returns a new Dec with the value of e^x, without mutating x.
