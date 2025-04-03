34ae4abba4bfbbfcc75f4eeb3615fbb2ac12d67f
Diego C <xmariachi@gmail.com>
2025-04-03 18:32:10 +0200

    fix param version test

--- x/emissions/types/params_test.go
+++ x/emissions/types/params_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestDefaultParams(t *testing.T) {
 	expectedParams := Params{
-		Version:                             "v7",
+		Version:                             "v10",
 		MinTopicWeight:                      alloraMath.MustNewDecFromString("100"),
 		RequiredMinimumStake:                cosmosMath.NewInt(10000),
 		RemoveStakeDelayWindow:              int64((60 * 60 * 24 * 7 * 3) / 3),
