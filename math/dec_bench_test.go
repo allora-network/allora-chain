c9fd4f13fcca1e41ef59a1037354fd58e754cee5
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-04-09 10:38:37 +0200

    refactor(math): rm unused exp10 func

--- math/dec_bench_test.go
+++ math/dec_bench_test.go
@@ -70,21 +70,6 @@ func BenchmarkExp(b *testing.B) {
 	}
 }
 
-func BenchmarkExp10(b *testing.B) {
-	for _, test := range benchExpData {
-		b.Run(fmt.Sprintf("input_%v", test), func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				b.StartTimer()
-				_, err := alloramath.Exp10(test)
-				if err != nil {
-					b.Fatal(err)
-				}
-				b.StopTimer()
-			}
-		})
-	}
-}
-
 func BenchmarkPow(b *testing.B) {
 	for _, d := range benchExpData {
 		for _, e := range benchExpData {
