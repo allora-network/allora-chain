d1343ae3897fb735c0757421a463d64b202afec7
Guilherme <guilherme1237@gmail.com>
2025-05-02 09:37:59 +0100

    Add missing upgrade dependencies

--- app/upgrades.go
+++ app/upgrades.go
@@ -8,6 +8,7 @@ import (
 	"github.com/allora-network/allora-chain/app/upgrades"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_10_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_11_0"
+	"github.com/allora-network/allora-chain/app/upgrades/v0_12_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_3_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_4_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_5_0"
@@ -27,6 +28,7 @@ var upgradeHandlers = []upgrades.Upgrade{
 	v0_9_0.Upgrade,
 	v0_10_0.Upgrade,
 	v0_11_0.Upgrade,
+	v0_12_0.Upgrade,
 	// Add more upgrade handlers here
 	// ...
 }
