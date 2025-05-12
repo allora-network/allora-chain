ed380e0246cd5aff4ab2761926b9845f3c4f424e
Diego C <xmariachi@gmail.com>
2025-05-12 15:40:34 +0200

    Mint module metrics + refactor on metric names (#819)
    <!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
    v           ✰  Thanks for creating a PR! You're awesome! ✰
    v Please note that maintainers will only review those PRs with a
    completed PR template.
    ☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->
    
    ## Purpose of Changes and their Description
    * Add mint metrics on events
    
    ## Are these changes tested and documented?
    
    - [x] If tested, please describe how. If not, why tests are not needed.
    -- unit and integration tests
    - [x] If documented, please describe where. If not, describe why docs
    are not needed. -- fixed docs on module `README`s
    - [x] Added to `Unreleased` section of `CHANGELOG.md`?
    
    ---------
    
    Signed-off-by: Diego C <xmariachi@gmail.com>
    Co-authored-by: Arnaud Mimart <33665250+amimart@users.noreply.github.com>

---
+++ x/mint/metrics/labels.go
@@ -0,0 +1,8 @@
+package metrics
+
+//nolint:gosec // These are event labels, not credentials
+const (
+	TOKENOMICS_SET_EVENT                = "tokenomics_set_event"
+	ECOSYSTEM_TOKEN_MINT_SET_EVENT      = "ecosystem_token_mint_set_event"
+	REWARD_CURRENT_BLOCK_EMISSION_EVENT = "reward_current_block_emission_event"
+)
