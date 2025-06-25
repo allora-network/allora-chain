49ba5019dbab8828c65ec2d7d5b6116571ca1c82
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-06-25 11:51:20 +0200

    feat: add dummy integration of scheduler in emissions

--- x/emissions/keeper/expected_keepers.go
+++ x/emissions/keeper/expected_keepers.go
@@ -2,9 +2,12 @@ package keeper
 
 import (
 	"context"
+	"time"
 
 	"cosmossdk.io/core/address"
+	"github.com/allora-network/allora-chain/x/scheduler/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/gogoproto/proto"
 )
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
@@ -29,4 +32,8 @@ type BankKeeper interface {
 	SendCoinsFromAccountToModule(ctx context.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 
 	BlockedAddr(addr sdk.AccAddress) bool
+}
+
+type SchedulerKeeper interface {
+	ScheduleTask(ctx context.Context, typename string, id types.TaskID, args proto.Message, at time.Time) error
 }
