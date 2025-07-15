839be16537922378cffec9f500a77276684c54b6
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-07-15 17:46:23 +0200

    feat(x/scheduler): add state design for task storage

--- x/scheduler/types/keys.go
+++ x/scheduler/types/keys.go
@@ -1,9 +1,17 @@
 package types
 
+import "cosmossdk.io/collections"
+
 const (
 	// ModuleName defines the module name.
 	ModuleName = "scheduler"
 
 	// StoreKey defines the primary module store key.
 	StoreKey = ModuleName
+)
+
+var (
+	TasksKeyPrefix       = collections.NewPrefix(0) // TasksKeyPrefix stores tasks.
+	TasksByTypeKeyPrefix = collections.NewPrefix(1) // TasksByTypeKeyPrefix stores task ids per task type.
+	TasksSchedulePrefix  = collections.NewPrefix(2) // TasksRunKeyPrefix stores tasks scheduled run.
 )
