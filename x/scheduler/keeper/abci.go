c6171541b3ee85be7f88d8c569e6d96c13778ae6
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-08-29 13:49:39 +0200

    feat(x/scheduler): implement task execution logic

--- x/scheduler/keeper/abci.go
+++ x/scheduler/keeper/abci.go
@@ -3,7 +3,10 @@ package keeper
 import (
 	"context"
 
+	"cosmossdk.io/collections"
+	"cosmossdk.io/errors"
 	"github.com/allora-network/allora-chain/x/scheduler/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k *Keeper) applyArbitrageDecision(ctx context.Context, task types.TaskID, decision types.ArbitrageDecision) (err error) {
@@ -19,4 +22,37 @@ func (k *Keeper) applyArbitrageDecision(ctx context.Context, task types.TaskID, 
 		err = k.PausePeriodicTask(ctx, task)
 	}
 	return
+}
+
+func (k *Keeper) runTask(ctx context.Context, task types.Task, handler types.TaskHandler) error {
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	task.RunCount++
+	if err := handler.Run(ctx, k.cdc, task.Id, task.Args, task.RunCount); err != nil {
+		return errors.Wrapf(types.ErrTaskExecution, "run func failed for task '%s' of type '%s': %s", task.Id, handler.Typename(), err)
+	}
+
+	// if the task is periodic, update its last run time and next run time, and reschedule it.
+	if task.Interval != nil {
+		if err := k.tasksSchedule.Remove(ctx, collections.Join3(task.Typename, task.NextRunAt, task.Id)); err != nil {
+			return errors.Wrapf(types.ErrTaskExecution, "couldn't reschedule periodic task '%s' of type '%s': %s", task.Id, handler.Typename(), err)
+		}
+
+		runTime := sdkCtx.BlockTime()
+		task.LastRunAt = &runTime
+		task.NextRunAt = runTime.Add(*task.Interval)
+		if err := k.tasks.Set(ctx, task.Id, task); err != nil {
+			return errors.Wrapf(types.ErrTaskExecution, "couldn't reschedule periodic task '%s' of type '%s': %s", task.Id, handler.Typename(), err)
+		}
+
+		if err := k.tasksSchedule.Set(ctx, collections.Join3(task.Typename, task.NextRunAt, task.Id)); err != nil {
+			return errors.Wrapf(types.ErrTaskExecution, "couldn't reschedule periodic task '%s' of type '%s': %s", task.Id, handler.Typename(), err)
+		}
+	} else {
+		if err := k.CancelTask(ctx, task.Id); err != nil {
+			return errors.Wrapf(types.ErrTaskExecution, "couldn't remove task '%s' of type '%s': %s", task.Id, handler.Typename(), err)
+		}
+	}
+
+	return nil
 }
--- x/scheduler/types/errors.go
+++ x/scheduler/types/errors.go
@@ -6,4 +6,6 @@ var (
 	ErrInvalidTaskHandler   = errors.Register(ModuleName, 2, "invalid task handler")
 	ErrInvalidTask          = errors.Register(ModuleName, 3, "invalid task")
 	ErrInvalidTaskArguments = errors.Register(ModuleName, 4, "invalid task arguments")
+	ErrTaskArbitrage        = errors.Register(ModuleName, 5, "task arbitrage failed")
+	ErrTaskExecution        = errors.Register(ModuleName, 6, "task execution failed")
 )
