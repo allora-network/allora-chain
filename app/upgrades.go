1b4c5c577299af99add48475f24fcfbb79913f73
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-10-02 16:16:40 +0200

    feat(app): register v0.14.0 upgrade with new scheduler store

--- app/upgrades.go
+++ app/upgrades.go
@@ -9,6 +9,7 @@ import (
 	"github.com/allora-network/allora-chain/app/upgrades/v0_10_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_11_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_12_0"
+	"github.com/allora-network/allora-chain/app/upgrades/v0_14_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_3_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_4_0"
 	"github.com/allora-network/allora-chain/app/upgrades/v0_5_0"
@@ -29,6 +30,7 @@ var upgradeHandlers = []upgrades.Upgrade{
 	v0_10_0.Upgrade,
 	v0_11_0.Upgrade,
 	v0_12_0.Upgrade,
+	v0_14_0.Upgrade,
 	// Add more upgrade handlers here
 	// ...
 }
