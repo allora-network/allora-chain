75dc1a3c26a458867436083dee2c103851bdd2c4
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-10-02 16:11:19 +0200

    test(x/schedule): cover genesis export/import

--- x/scheduler/keeper/genesis.go
+++ x/scheduler/keeper/genesis.go
@@ -3,15 +3,20 @@ package keeper
 import (
 	"context"
 
-	"cosmossdk.io/collections"
+	"cosmossdk.io/errors"
 	"github.com/allora-network/allora-chain/x/scheduler/types"
 )
 
 func (k *Keeper) InitGenesis(ctx context.Context, state types.GenesisState) {
 	for _, task := range state.Tasks {
+		if _, ok := k.handlersByTypename[task.Typename]; !ok {
+			panic(errors.Wrapf(types.ErrInvalidTask, "unknown task typename '%s'", task.Typename))
+		}
+
 		if err := k.tasks.Set(ctx, task.Id, task); err != nil {
 			panic(err)
 		}
+
 		if key := getTaskScheduleKey(task); key != nil {
 			if err := k.tasksSchedule.Set(ctx, *key); err != nil {
 				panic(err)
@@ -22,7 +27,7 @@ func (k *Keeper) InitGenesis(ctx context.Context, state types.GenesisState) {
 
 func (k *Keeper) ExportGenesis(ctx context.Context) types.GenesisState {
 	var tasks []types.Task
-	it, err := k.tasks.Iterate(ctx, &collections.Range[types.TaskID]{})
+	it, err := k.tasks.Iterate(ctx, nil)
 	if err != nil {
 		panic(err)
 	}
