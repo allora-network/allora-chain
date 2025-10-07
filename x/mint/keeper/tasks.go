1c637f2650c6969455ec3e31826e9dd922998f32
Guilherme <guilherme1237@gmail.com>
2025-10-07 16:25:20 +0100

    Add migration

--- x/mint/keeper/tasks.go
+++ x/mint/keeper/tasks.go
@@ -29,78 +29,26 @@ func (k *Keeper) TaskHandlers() schedulertypes.TaskHandlers {
 func (k *Keeper) executeEmissionRecalculationTask(ctx context.Context) error {
 	sdkCtx := sdk.UnwrapSDKContext(ctx)
 
-	moduleParams, err := k.Params.Get(ctx)
-	if err != nil {
-		return err
-	}
-
-	// if emissions are not enabled, do nothing
-	if !moduleParams.EmissionEnabled {
-		return nil
-	}
-
-	// Get the balance of the "ecosystem" module account
-	ecosystemBalance, err := k.GetEcosystemBalance(ctx, moduleParams.MintDenom)
-	if err != nil {
-		return err
-	}
-
-	blockHeight := uint64(sdkCtx.BlockHeight())
-
-	ecosystemMintSupplyRemaining, err := k.GetEcosystemMintSupplyRemaining(ctx, moduleParams)
-	if err != nil {
-		return err
-	}
-
-	blocksPerMonth, err := k.GetParamsBlocksPerMonth(ctx)
-	if err != nil {
-		return err
-	}
-
-	vPercentADec, err := k.GetValidatorsVsAlloraPercentReward(ctx)
-	if err != nil {
-		return err
-	}
-
-	vPercent, err := vPercentADec.SdkLegacyDec()
-	if err != nil {
-		return err
-	}
-
-	// Always recalculate the target emission when the task runs (monthly via scheduler)
-	// WARNING: After Calling RecalculateTargetEmission,
-	// PreviousRewardEmissionPerUnitStakedToken and PreviousBlockEmission
-	// are set to new values. If later in begin blocker you need to use these
-	// you should get them first before this function is called!!
-	currentBlockEmission, _, err := RecalculateTargetEmission(
-		sdkCtx,
-		*k,
-		blockHeight,
-		blocksPerMonth,
-		moduleParams,
-		ecosystemBalance,
-		ecosystemMintSupplyRemaining,
-		vPercent,
-	)
+	blockEmission, err := k.RecalculateEmission(ctx)
 	if err != nil {
 		return err
 	}
 
 	k.Logger(ctx).Info("Emission recalculation task executed",
-		"blockHeight", blockHeight,
-		"blockEmission", currentBlockEmission.String(),
-		"recalculated", true,
+		"blockHeight", sdkCtx.BlockHeight(),
+		"blockEmission", blockEmission.String(),
 	)
 
 	return nil
 }
 
-// ScheduleEmissionRecalculationTask schedules the emission recalculation task to run every 6 months
+// ScheduleEmissionRecalculationTask schedules the emission recalculation task to run roughly every month
 func (k *Keeper) ScheduleEmissionRecalculationTask(ctx context.Context, schedulerKeeper types.SchedulerKeeper) error {
-	// Calculate 6 months duration (approximately 182.5 days * 24 hours * 60 minutes * 60 seconds)
-	sixMonthDuration := time.Hour * 24 * 182 // 182 days ≈ 6 months
+	// Approximate one month as 30 days. The exact cadence is recalculated by the
+	// mint module based on on-chain parameters when the task runs.
+	oneMonthDuration := time.Hour * 24 * 30
 
-	k.Logger(ctx).Info("scheduling emission recalculation task", "interval", sixMonthDuration)
+	k.Logger(ctx).Info("scheduling emission recalculation task", "interval", oneMonthDuration)
 
 	// Use empty struct since this task doesn't need any arguments
 	args := &types.EmissionRecalculationTaskArgs{}
@@ -110,8 +58,8 @@ func (k *Keeper) ScheduleEmissionRecalculationTask(ctx context.Context, schedule
 		types.TaskEmissionRecalculation,
 		schedulertypes.TaskID("emission_recalculation"),
 		args,
-		schedulertypes.ScheduleIn(sixMonthDuration),
-		schedulertypes.ScheduleEvery(&sixMonthDuration),
+		schedulertypes.ScheduleIn(oneMonthDuration),
+		schedulertypes.ScheduleEvery(&oneMonthDuration),
 		schedulertypes.WithRelativeScheduling(),
 	)
 }
