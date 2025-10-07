ef99dcc9692307b970f00111bd27fda5f35c67cc
Guilherme <guilherme1237@gmail.com>
2025-10-07 16:25:20 +0100

    Add migration

--- app/upgrades/v0_13_0/upgrades.go
+++ app/upgrades/v0_13_0/upgrades.go
@@ -2,6 +2,7 @@ package v0_13_0 //nolint:revive // var-naming: don't use an underscore in packag
 
 import (
 	"context"
+	"strings"
 
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
@@ -9,6 +10,7 @@ import (
 	"github.com/allora-network/allora-chain/app/upgrades"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/pkg/errors"
 )
 
 const (
@@ -24,7 +26,7 @@ var Upgrade = upgrades.Upgrade{
 func CreateUpgradeHandler(
 	moduleManager *module.Manager,
 	configurator module.Configurator,
-	_ *keepers.AppKeepers,
+	appKeepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
@@ -32,6 +34,23 @@ func CreateUpgradeHandler(
 		vm, err := moduleManager.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
+		}
+
+		if appKeepers != nil && appKeepers.SchedulerKeeper != nil {
+			mintTaskHandlers := appKeepers.MintKeeper.TaskHandlers()
+			if err := appKeepers.SchedulerKeeper.RegisterTaskHandlers(mintTaskHandlers); err != nil {
+				if !errors.Is(err, schedulertypes.ErrInvalidTaskHandler) || !strings.Contains(err.Error(), "duplicate task handler") {
+					sdkCtx.Logger().Error("failed to register mint task handlers", "err", err)
+					return vm, err
+				}
+			}
+
+			if err := appKeepers.MintKeeper.ScheduleEmissionRecalculationTask(ctx, appKeepers.SchedulerKeeper); err != nil {
+				sdkCtx.Logger().Error("failed to schedule emission recalculation task", "err", err)
+				return vm, err
+			}
+
+			sdkCtx.Logger().Info("scheduled emission recalculation task")
 		}
 
 		sdkCtx.Logger().Info("MIGRATIONS COMPLETED")
