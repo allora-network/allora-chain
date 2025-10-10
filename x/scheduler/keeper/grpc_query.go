74835977a7489fb310f2911b1087b1a25dc7edf7
Arnaud Mimart <33665250+amimart@users.noreply.github.com>
2025-10-10 09:18:59 +0200

    fest(x/scheduler): implements tasks rpc query

---
+++ x/scheduler/keeper/grpc_query.go
@@ -0,0 +1,69 @@
+package keeper
+
+import (
+	"context"
+
+	"cosmossdk.io/collections"
+	collutils "github.com/allora-network/allora-chain/utils/collections"
+	"github.com/allora-network/allora-chain/x/scheduler/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Querier is used as Keeper will have duplicate methods if used directly, and gRPC names take precedence over keeper
+type Querier struct {
+	*Keeper
+}
+
+var _ types.QueryServer = Querier{}
+
+func NewQuerier(keeper *Keeper) Querier {
+	return Querier{Keeper: keeper}
+}
+
+func (q Querier) Tasks(ctx context.Context, req *types.QueryTasksRequest) (*types.QueryTasksResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "empty request")
+	}
+
+	var tasks []types.Task
+	var pageRes *query.PageResponse
+	var err error
+	if req.Typename == "" {
+		tasks, pageRes, err = q.getAllTasks(ctx, req.Pagination)
+	} else {
+		tasks, pageRes, err = q.getTasksByType(ctx, req.Typename, req.Pagination)
+	}
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryTasksResponse{
+		Tasks:      tasks,
+		Pagination: pageRes,
+	}, nil
+}
+
+func (q Querier) getAllTasks(ctx context.Context, pagination *query.PageRequest) ([]types.Task, *query.PageResponse, error) {
+	return query.CollectionPaginate(
+		ctx,
+		q.tasks,
+		pagination,
+		func(key types.TaskID, value types.Task) (types.Task, error) {
+			return value, nil
+		},
+	)
+}
+
+func (q Querier) getTasksByType(ctx context.Context, typename string, pagination *query.PageRequest) ([]types.Task, *query.PageResponse, error) {
+	return query.CollectionPaginate(
+		ctx,
+		collutils.WrapMultiIndexToCollection(q.tasks.Indexes.ByType),
+		pagination,
+		func(key collections.Pair[string, types.TaskID], _ collections.NoValue) (types.Task, error) {
+			return q.tasks.Get(ctx, key.K2())
+		},
+		query.WithCollectionPaginationPairPrefix[string, types.TaskID](typename),
+	)
+}
