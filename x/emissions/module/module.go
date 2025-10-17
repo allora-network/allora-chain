d38014dba8a983f7c587c6513c2072db5282de92
Diego C <xmariachi@gmail.com>
2025-10-18 01:48:54 +0200

    remove v10 on emissions

--- x/emissions/module/module.go
+++ x/emissions/module/module.go
@@ -13,7 +13,6 @@ import (
 	v6 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v6"
 	v7 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v7"
 	v8 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v8"
-	v9 "github.com/allora-network/allora-chain/x/emissions/api/emissions/v9"
 	"github.com/allora-network/allora-chain/x/emissions/keeper"
 	"github.com/allora-network/allora-chain/x/emissions/keeper/msgserver"
 	"github.com/allora-network/allora-chain/x/emissions/keeper/queryserver"
@@ -72,7 +71,6 @@ func (AppModule) RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	v6.RegisterTypes(cdc)
 	v7.RegisterTypes(cdc)
 	v8.RegisterTypes(cdc)
-	v9.RegisterTypes(cdc)
 }
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the state module.
@@ -92,7 +90,6 @@ func (AppModule) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
 	v6.RegisterInterfaces(registry)
 	v7.RegisterInterfaces(registry)
 	v8.RegisterInterfaces(registry)
-	v9.RegisterInterfaces(registry)
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
