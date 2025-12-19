18b1e4576d4ec82683697786dde2ca040e6e9cfd
Diego C <xmariachi@gmail.com>
2025-12-19 11:38:30 +0100

    Initial mint to allorarewards (#895)
    <!-- < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < < ☺
    v           ✰  Thanks for creating a PR! You're awesome! ✰
    v Please note that maintainers will only review those PRs with a
    completed PR template.
    ☺ > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > > >  -->
    
    ## Purpose of Changes and their Description
    Compensate initial amount to rewards to ensure there is a buffer for
    potential swings in rewards.
    
    
    ## Are these changes tested and documented?
    
    - [x] If tested, please describe how. If not, why tests are not needed.
    -- run the upgrade test
    - [ ] If documented, please describe where. If not, describe why docs
    are not needed. -- no need, no functionality added
    - [ ] Added to `Unreleased` section of `CHANGELOG.md`? -- no need , no
    functionality added
    
    ---------
    
    Signed-off-by: Diego C <xmariachi@gmail.com>
    Co-authored-by: Arnaud Mimart <33665250+amimart@users.noreply.github.com>

--- app/upgrades/v0_15_0/upgrades.go
+++ app/upgrades/v0_15_0/upgrades.go
@@ -2,11 +2,17 @@ package v0_15_0 //nolint:revive // var-naming: don't use an underscore in packag
 
 import (
 	"context"
+	"fmt"
 
+	errorsmod "cosmossdk.io/errors"
+	"cosmossdk.io/math"
 	storetypes "cosmossdk.io/store/types"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
 	"github.com/allora-network/allora-chain/app/keepers"
+	"github.com/allora-network/allora-chain/app/params"
 	"github.com/allora-network/allora-chain/app/upgrades"
+	emissionstypes "github.com/allora-network/allora-chain/x/emissions/types"
+	minttypes "github.com/allora-network/allora-chain/x/mint/types"
 	schedulertypes "github.com/allora-network/allora-chain/x/scheduler/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -25,7 +31,7 @@ var Upgrade = upgrades.Upgrade{
 func CreateUpgradeHandler(
 	moduleManager *module.Manager,
 	configurator module.Configurator,
-	_ *keepers.AppKeepers,
+	keepers *keepers.AppKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx context.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
 		sdkCtx := sdk.UnwrapSDKContext(ctx)
@@ -33,6 +39,36 @@ func CreateUpgradeHandler(
 		vm, err := moduleManager.RunMigrations(ctx, configurator, vm)
 		if err != nil {
 			return vm, err
+		}
+
+		amount, ok := math.NewIntFromString("1000000000000000000000") // 1,000 ALLO
+		if !ok {
+			return nil, fmt.Errorf("could not convert amount to int")
+		}
+		coins := sdk.NewCoins(sdk.NewCoin(params.BaseCoinUnit, amount))
+
+		sdkCtx.Logger().Info("Minting new coins", "coins", coins)
+		if err := keepers.BankKeeper.MintCoins(ctx, minttypes.ModuleName, coins); err != nil {
+			return nil, err
+		}
+
+		sdkCtx.Logger().Info(
+			"Transferring newly minted coins to allorarewards module account",
+			"coins", coins,
+			"module", emissionstypes.AlloraRewardsAccountName,
+		)
+		if err := keepers.BankKeeper.SendCoinsFromModuleToModule(
+			ctx,
+			minttypes.ModuleName,
+			emissionstypes.AlloraRewardsAccountName,
+			coins,
+		); err != nil {
+			return nil, err
+		}
+
+		err = keepers.MintKeeper.AddEcosystemTokensMinted(ctx, coins.AmountOf(params.BaseCoinUnit))
+		if err != nil {
+			return nil, errorsmod.Wrap(err, "could not add ecosystem tokens minted")
 		}
 
 		sdkCtx.Logger().Info("MIGRATIONS COMPLETED")
