92898e8398ae945c7789f973ba0d61925fdae2dc
Diego C <xmariachi@gmail.com>
2026-01-08 17:13:08 +0100

    Update x/emissions/keeper/keeper.go
    Co-authored-by: Yung Spook <spooktheducks@protonmail.com>
    Signed-off-by: Diego C <xmariachi@gmail.com>

--- x/emissions/keeper/keeper.go
+++ x/emissions/keeper/keeper.go
@@ -3055,6 +3055,8 @@ func (k *Keeper) GetReputerInfo(ctx sdk.Context, reputerKey ActorId) (types.Offc
 }
 
 // UpdateReputerOwner updates the payout owner associated with a reputer node.
+// Returns:
+//     - (string) old owner
 func (k *Keeper) UpdateReputerOwner(ctx context.Context, reputer ActorId, newOwner string) (string, error) {
 	if err := types.ValidateBech32(reputer); err != nil {
 		return "", errorsmod.Wrap(err, "reputer validation failed")
