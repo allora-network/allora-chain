a00f011591567e49f5adcef28bbd4b52767443e6
zale144 <aleksandar.sukovic@gmail.com>
2026-03-04 13:19:06 +0100

    PR fixes

--- x/emissions/keeper/keeper.go
+++ x/emissions/keeper/keeper.go
@@ -5107,7 +5107,7 @@ func (k *Keeper) RegisterEpochLabel(
 		}
 	}
 
-	newID := LabelId(len(registry.Labels) + 1)
+	newID := LabelId(uint32(len(registry.Labels)) + 1)
 
 	registry.Labels = append(registry.Labels, &types.TopicLabel{
 		Id:   newID,
