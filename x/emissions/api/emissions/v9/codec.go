226e982f40814484fa6844b0ab2e669d0a0177a2
Diego C <xmariachi@gmail.com>
2026-04-17 21:31:35 +0200

    codec.go in line with vX (x<10) changes

---
+++ x/emissions/api/emissions/v9/codec.go
@@ -0,0 +1,47 @@
+package emissionsv9
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+//nolint:exhaustruct
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&UpdateParamsRequest{},
+		&CreateNewTopicRequest{},
+		&RegisterRequest{},
+		&RemoveRegistrationRequest{},
+		&AddStakeRequest{},
+		&RemoveStakeRequest{},
+		&CancelRemoveStakeRequest{},
+		&DelegateStakeRequest{},
+		&RewardDelegateStakeRequest{},
+		&RemoveDelegateStakeRequest{},
+		&CancelRemoveDelegateStakeRequest{},
+		&FundTopicRequest{},
+		&AddToWhitelistAdminRequest{},
+		&RemoveFromWhitelistAdminRequest{},
+		&InsertWorkerPayloadRequest{},
+		&InsertReputerPayloadRequest{},
+		&AddToGlobalWhitelistRequest{},
+		&RemoveFromGlobalWhitelistRequest{},
+		&EnableTopicWorkerWhitelistRequest{},
+		&DisableTopicWorkerWhitelistRequest{},
+		&EnableTopicReputerWhitelistRequest{},
+		&DisableTopicReputerWhitelistRequest{},
+		&AddToTopicCreatorWhitelistRequest{},
+		&RemoveFromTopicCreatorWhitelistRequest{},
+		&AddToTopicWorkerWhitelistRequest{},
+		&RemoveFromTopicWorkerWhitelistRequest{},
+		&AddToTopicReputerWhitelistRequest{},
+		&RemoveFromTopicReputerWhitelistRequest{},
+	)
+}
+
+// So we need to register types like:
+func RegisterTypes(registry *codec.LegacyAmino) {
+	// Internal types used by requests
+	registry.RegisterConcrete(&OptionalParams{}, "emissions/v9/OptionalParams", nil) //nolint:exhaustruct
+}
